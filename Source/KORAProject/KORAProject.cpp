3f9f2e17c02d203ad6ad85ff88e9ca406f48dc03
SungPhilJoon <soung2849@gmail.com>
2025-10-24 17:07:02 +0900

    KORA 프로젝트 생성

---
+++ Source/KORAProject/KORAProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KORAProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KORAProject, "KORAProject" );
