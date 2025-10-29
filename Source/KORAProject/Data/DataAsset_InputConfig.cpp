deec40f52dc63551cbe4f4b88a3d983f33ed1da8
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 20:01:40 +0900

    입력 구성 데이터 에셋에 NativeInputActions 검색 함수 FindNativeInputActionByTag 추가 및 관련 구조체 FKRInputActionConfig 정의

--- Source/KORAProject/Data/DataAsset_InputConfig.cpp
+++ Source/KORAProject/Data/DataAsset_InputConfig.cpp
@@ -1,5 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Data/DataAsset_InputConfig.h"
 
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag) const
+{
+	for (const FKRInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	
+	return nullptr;
+}
