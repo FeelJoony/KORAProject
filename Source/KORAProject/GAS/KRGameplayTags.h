5d69fd01e67461dc555b23a92eaf99b6e4d88fdf
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 17:56:31 +0900

    GameplayTags 관련 클래스 KRGameplayTags 추가

---
+++ Source/KORAProject/GAS/KRGameplayTags.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace KRGameplayTags
+{
+	/* Input Tags */
+	KORAPROJECT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Move);
+	KORAPROJECT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Look);
+}
