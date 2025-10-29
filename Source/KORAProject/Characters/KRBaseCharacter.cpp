238e11be9e8fd6d7810733b7a49bb01c1e6c79eb
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 17:55:58 +0900

    기본 캐릭터 클래스 KRBaseCharacter 추가

---
+++ Source/KORAProject/Characters/KRBaseCharacter.cpp
@@ -0,0 +1,10 @@
+#include "Characters/KRBaseCharacter.h"
+
+AKRBaseCharacter::AKRBaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	GetMesh()->bReceivesDecals = false;
+}
+
---
+++ Source/KORAProject/Characters/KRBaseCharacter.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "KRBaseCharacter.generated.h"
+
+UCLASS()
+class KORAPROJECT_API AKRBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AKRBaseCharacter();
+
+
+};
