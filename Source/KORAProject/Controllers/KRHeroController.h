3348092e92c9a23796468c4f5e9fb2fcf4b5b0f2
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 17:56:19 +0900

    플레이어 캐릭터 클래스 KRHeroCharacter 및 컨트롤러 KRHeroController 추가

---
+++ Source/KORAProject/Controllers/KRHeroController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "KRHeroController.generated.h"
+
+
+UCLASS()
+class KORAPROJECT_API AKRHeroController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
