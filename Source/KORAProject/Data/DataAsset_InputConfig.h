dea3f5bbc96bbf22cf59356dbcdb4a67acfc7d70
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 20:01:40 +0900

    입력 구성 데이터 에셋에 NativeInputActions 검색 함수 FindNativeInputActionByTag 추가 및 관련 구조체 FKRInputActionConfig 정의

--- Source/KORAProject/Data/DataAsset_InputConfig.h
+++ Source/KORAProject/Data/DataAsset_InputConfig.h
@@ -1,17 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_InputConfig.generated.h"
 
-/**
- * 
- */
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FKRInputActionConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "Input"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+
 UCLASS()
 class KORAPROJECT_API UDataAsset_InputConfig : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FKRInputActionConfig> NativeInputActions;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FKRInputActionConfig> AbilityInputActions;
+	
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag) const;
 	
 };
