e949ada480678b5bd4e0cef1b7ef4d41bbca14f5
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 20:01:04 +0900

    입력 컴포넌트 클래스 KRInputComponent 추가

---
+++ Source/KORAProject/Components/Input/KRInputComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "KRInputComponent.generated.h"
+
+
+UCLASS()
+class KORAPROJECT_API UKRInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
