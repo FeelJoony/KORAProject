a1e51d0ea97953aa4399441eddceff5d6352278c
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-29 17:56:14 +0900

    플레이어 캐릭터 KRHeroCharacter 클래스 추가

---
+++ Source/KORAProject/Characters/KRHeroCharacter.cpp
@@ -0,0 +1,35 @@
+#include "Characters/KRHeroCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AKRHeroCharacter::AKRHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 200.f;
+	SpringArm->SocketOffset = FVector(0.f, 55.f, 65.f);
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	
+}
+
+void AKRHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
