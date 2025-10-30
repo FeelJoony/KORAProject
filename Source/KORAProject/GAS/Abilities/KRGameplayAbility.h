266f6d127e1f50d771bb57262f14829910d4b46b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-30 19:52:53 +0900

    KRGameplayAbility 클래스 추가

---
+++ Source/KORAProject/GAS/Abilities/KRGameplayAbility.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "KRGameplayAbility.generated.h"
+
+UCLASS()
+class KORAPROJECT_API UKRGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
