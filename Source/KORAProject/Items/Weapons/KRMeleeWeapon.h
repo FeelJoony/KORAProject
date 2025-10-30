43684aa3a1a038895cce95f5855e30d2a70e49d7
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-30 23:21:22 +0900

    KRMeleeWeapon 클래스 추가

---
+++ Source/KORAProject/Items/Weapons/KRMeleeWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/KRWeaponBase.h"
+#include "KRMeleeWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API AKRMeleeWeapon : public AKRWeaponBase
+{
+	GENERATED_BODY()
+	
+};
