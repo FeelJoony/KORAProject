807a2b379d043654b6018080942c38e220adcfb0
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-30 23:20:58 +0900

    KRGameplayAbility 클래스에 AbilityActivationPolicy 추가 및 OnGiveAbility, EndAbility 로직 구현

--- Source/KORAProject/GAS/Abilities/KRGameplayAbility.h
+++ Source/KORAProject/GAS/Abilities/KRGameplayAbility.h
@@ -4,9 +4,22 @@
 #include "Abilities/GameplayAbility.h"
 #include "KRGameplayAbility.generated.h"
 
+UENUM(BlueprintType)
+enum class EKRAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
 UCLASS()
 class KORAPROJECT_API UKRGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "KRAbility")
+	EKRAbilityActivationPolicy AbilityActivationPolicy = EKRAbilityActivationPolicy::OnTriggered;
 };
