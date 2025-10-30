399c5d6f195e24911271d7098f991110e67be183
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-30 23:20:39 +0900

    KRPlayerState 클래스에 AbilitySystemComponent 추가 및 초기화 로직 구현

--- Source/KORAProject/Player/KRPlayerState.cpp
+++ Source/KORAProject/Player/KRPlayerState.cpp
@@ -1,5 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Player/KRPlayerState.h"
+#include "GAS/KRPlayerAbilitySystemComponent.h"
+#include "AbilitySystemComponent.h"
+#include "GameFramework/Pawn.h"
 
+AKRPlayerState::AKRPlayerState()
+{
+	KRPlayerASC = CreateDefaultSubobject<UKRPlayerAbilitySystemComponent>(TEXT("ASC"));
+	KRPlayerASC->SetIsReplicated(true);
+	KRPlayerASC->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+}
+
+UAbilitySystemComponent* AKRPlayerState::GetAbilitySystemComponent() const
+{
+	return KRPlayerASC;
+}
+
+
+void AKRPlayerState::InitializeAbilitySystemForPawn(APawn* NewPawn)
+{
+	if (!KRPlayerASC) return;
+
+	KRPlayerASC->InitAbilityActorInfo(this, NewPawn);
+
+	if (HasAuthority() && !bStartupGiven)
+	{
+		KRPlayerASC->ApplyInitialEffects();
+		KRPlayerASC->GiveInitialAbilities();
+		KRPlayerASC->GiveInputAbilities();
+		bStartupGiven = true;
+	}
+}
--- Source/KORAProject/Player/KRPlayerState.h
+++ Source/KORAProject/Player/KRPlayerState.h
@@ -1,17 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
 #include "KRPlayerState.generated.h"
 
-/**
- * 
- */
+class APawn;
+class UKRPlayerAbilitySystemComponent;
+
 UCLASS()
-class KORAPROJECT_API AKRPlayerState : public APlayerState
+class KORAPROJECT_API AKRPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	AKRPlayerState();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	void InitializeAbilitySystemForPawn(APawn* NewPawn);
+
+	UFUNCTION(BlueprintPure, Category = "GAS")
+	UKRPlayerAbilitySystemComponent* GetKRPlayerASC() const {return KRPlayerASC;}
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UKRPlayerAbilitySystemComponent> KRPlayerASC;
+
+	bool bStartupGiven = false;
 };
