66e4abd5f5c890ee5bde9f7e9b33f8a22149c64d
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-30 23:21:06 +0900

    KRBaseCharacter 클래스에 AbilitySystemInterface 구현 및 AbilitySystemComponent 캐싱 로직 추가

--- Source/KORAProject/Characters/KRBaseCharacter.h
+++ Source/KORAProject/Characters/KRBaseCharacter.h
@@ -1,16 +1,25 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "KRBaseCharacter.generated.h"
 
+class UAbilitySystemComponent;
+
 UCLASS()
-class KORAPROJECT_API AKRBaseCharacter : public ACharacter
+class KORAPROJECT_API AKRBaseCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AKRBaseCharacter();
 
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
+protected:
+	UPROPERTY(Transient, VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UAbilitySystemComponent> CachedASC = nullptr;
+
+	FORCEINLINE void SetCachedASC(UAbilitySystemComponent* InASC) { CachedASC = InASC; }
 };
