fea95b4978e7d258dd28e95c9982d19eef5fa594
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-31 14:22:03 +0900

    KRHeroMeleeWeapon 클래스 주석 제거 및 코드 정리

--- Source/KORAProject/Items/Weapons/KRHeroMeleeWeapon.h
+++ Source/KORAProject/Items/Weapons/KRHeroMeleeWeapon.h
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Items/Weapons/KRMeleeWeapon.h"
 #include "KRHeroMeleeWeapon.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class KORAPROJECT_API AKRHeroMeleeWeapon : public AKRMeleeWeapon
 {
