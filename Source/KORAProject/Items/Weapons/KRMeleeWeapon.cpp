5068ee141f7b13cbf6a0a09e6e4bb5607d0cac9d
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-31 14:21:56 +0900

    KRMeleeWeapon 클래스에 WeaponCollisionBox 컴포넌트 추가 및 초기화 로직 구현

--- Source/KORAProject/Items/Weapons/KRMeleeWeapon.cpp
+++ Source/KORAProject/Items/Weapons/KRMeleeWeapon.cpp
@@ -1,5 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Items/Weapons/KRMeleeWeapon.h"
+#include "Components/BoxComponent.h"
 
+AKRMeleeWeapon::AKRMeleeWeapon()
+{
+	WeaponCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollisionBox"));
+	WeaponCollisionBox->SetupAttachment(GetRootComponent());
+	WeaponCollisionBox->SetBoxExtent(FVector(20.f));
+	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
--- Source/KORAProject/Items/Weapons/KRMeleeWeapon.h
+++ Source/KORAProject/Items/Weapons/KRMeleeWeapon.h
@@ -1,17 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Items/Weapons/KRWeaponBase.h"
 #include "KRMeleeWeapon.generated.h"
 
-/**
- * 
- */
+class UBoxComponent;
+
 UCLASS()
 class KORAPROJECT_API AKRMeleeWeapon : public AKRWeaponBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AKRMeleeWeapon();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapons")
+	TObjectPtr<UBoxComponent> WeaponCollisionBox;
+
+public:
+	FORCEINLINE UBoxComponent* GetWeaponCollisionBox() const {return WeaponCollisionBox;}
 };
