b51f9b048de653ea68d6fb33c4da525eea59cbab
FeelJoony <soung2849@gmail.com>
2025-11-05 02:36:25 +0900

    KRGameInstance 추가 및 적용 KR User SubSystem 추가 KRContents 추가 및 KRContents를 상속하는 Inventory 클래스 추가

---
+++ Source/KORAProject/Contents/KRInventory.cpp
@@ -0,0 +1,19 @@
+#include "Contents/KRInventory.h"
+
+UKRInventory::UKRInventory()
+{
+}
+
+void UKRInventory::Initialize()
+{
+
+}
+
+void UKRInventory::SetUserData()
+{
+}
+
+void UKRInventory::DeInitialize()
+{
+
+}
