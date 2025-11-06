5bc3c7cf7e27eda2590555138bc546cf3ba612e1
천재우 <dofr1564@naver.com>
2025-11-07 00:20:29 +0900

    [feat] KRItemSubsystem 및 KRInventorySubsystem 추가
    아이템 데이터 테이블 로드 및 관리 기능을 갖춘 KRItemSubsystem과 인벤토리 관리의 기반이 되는 KRInventorySubsystem을 구현했습니다.

---
+++ Source/KORAProject/SubSystem/KRInventorySubsystem.cpp
@@ -0,0 +1,2 @@
+#include "Subsystem/KRInventorySubsystem.h"
+
---
+++ Source/KORAProject/SubSystem/KRInventorySubsystem.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "KRInventorySubsystem.generated.h"
+
+UCLASS()
+class KORAPROJECT_API UKRInventorySubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
