db3b35299d384799b20a19a80906f2a72d54c3b3
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-07 15:24:49 +0900

    PawnCombatComponent 클래스 및 관련 파일 추가

---
+++ Source/KORAProject/Components/Combat/PawnCombatComponent.cpp
@@ -0,0 +1,43 @@
+#include "Components/Combat/PawnCombatComponent.h"
+#include "Items/Weapons/KRWeaponBase.h"
+
+#include "KORADebugHelper.h"
+
+void UPawnCombatComponent::RegisterSpawnedWeapon(FGameplayTag InWeaponTagToRegister, AKRWeaponBase* InWeaponToRegister,
+	bool bRegisterAsEquippedWeapon)
+{
+	checkf(!CharacterCarriedWeaponMap.Contains(InWeaponTagToRegister), TEXT("A %s has already added carried weapon"), *InWeaponTagToRegister.ToString());
+	check(InWeaponToRegister);
+
+	CharacterCarriedWeaponMap.Emplace(InWeaponTagToRegister, InWeaponToRegister);
+
+	if (bRegisterAsEquippedWeapon)
+	{
+		CurrentEquippedWeaponTag = InWeaponTagToRegister;
+	}
+
+	const FString WeaponString = FString::Printf(TEXT("A weapon named: %s has been registered using the tag %s"),*InWeaponToRegister->GetName(),*InWeaponTagToRegister.ToString());
+	Debug::Print(WeaponString);	
+}
+
+AKRWeaponBase* UPawnCombatComponent::GetCharacterCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const
+{
+	if (CharacterCarriedWeaponMap.Contains(InWeaponTagToGet))
+	{
+		if (AKRWeaponBase* const* FoundWeapon = CharacterCarriedWeaponMap.Find(InWeaponTagToGet))
+		{
+			return *FoundWeapon;
+		}
+	}
+	return nullptr;
+}
+
+AKRWeaponBase* UPawnCombatComponent::GetCharacterCurrentEquippedWeapon() const
+{
+	if (!CurrentEquippedWeaponTag.IsValid())
+	{
+		return nullptr;
+	}
+
+	return GetCharacterCarriedWeaponByTag(CurrentEquippedWeaponTag);
+}
