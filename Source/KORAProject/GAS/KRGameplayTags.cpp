b6004d98edf4ee825bff7d9eb8080b72051d0a30
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-07 15:25:38 +0900

    GameplayTags에 무기 장착 및 해제 관련 태그 추가

--- Source/KORAProject/GAS/KRGameplayTags.cpp
+++ Source/KORAProject/GAS/KRGameplayTags.cpp
@@ -3,12 +3,35 @@
 namespace KRGameplayTags
 {
 	/* Input Tags */
-	UE_DEFINE_GAMEPLAY_TAG(Input_Move, "Input.Move");
-	UE_DEFINE_GAMEPLAY_TAG(Input_Look, "Input.Look");
-
+	UE_DEFINE_GAMEPLAY_TAG(Input_Move,			"Input.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Look,			"Input.Look");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Equip_Sword,	"Input.Equip.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Equip_Gun,		"Input.Equip.Gun");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Unequip_Sword, "Input.Unequip.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Unequip_Gun,	"Input.Unequip.Gun");
+	
+	
 	/* State Tags */
 	UE_DEFINE_GAMEPLAY_TAG(State_Acting_Attaking, "State.Acting.Attaking");
+
+
+	/* Player Tags */
+	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Sword, "Player.Weapon.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Gun,	"Player.Weapon.Gun");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Can_Step,		"Player.Can.Step");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Can_Jump,		"Player.Can.Jump");
+
+	/* Event Tags */
+	UE_DEFINE_GAMEPLAY_TAG(Event_Equip_Sword,	"Event.Equip.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Unequip_Sword, "Event.Unequip.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Equip_Gun,		"Event.Equip.Gun");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Unequip_Gun,	"Event.Unequip.Gun");
+
+	/* Ability Tags */
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Equip_Sword,	  "Ability.Equip.Sword");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Unequip_Sword, "Ability.Unequip.Sword");	
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Equip_Gun,	  "Ability.Equip.Gun");
+	UE_DEFINE_GAMEPLAY_TAG(Ability_Unequip_Gun,	  "Ability.Unequip.Gun");
+
 	
-	UE_DEFINE_GAMEPLAY_TAG(Player_Can_Step, "Player.Can.Step");
-	UE_DEFINE_GAMEPLAY_TAG(Player_Can_Jump, "Player.Can.Jump");
 }
