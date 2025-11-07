e15c14ccbfd31bf49962218d8e764125fe196003
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-07 15:25:57 +0900

    KRHeroMeleeWeapon에 HeroWeaponData 데이터 추가

--- Source/KORAProject/Items/Weapons/KRHeroMeleeWeapon.h
+++ Source/KORAProject/Items/Weapons/KRHeroMeleeWeapon.h
@@ -2,11 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "Items/Weapons/KRMeleeWeapon.h"
+#include "KORATypes/KRStructTypes.h"
 #include "KRHeroMeleeWeapon.generated.h"
 
 UCLASS()
 class KORAPROJECT_API AKRHeroMeleeWeapon : public AKRMeleeWeapon
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WeaponData")
+	FKRHeroWeaponData HeroWeaponData;
 };
