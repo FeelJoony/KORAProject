55cbe34b09540a9686f33a0d7d123282b852e137
Juyee Myeong <juyee.ming@gmail.com>
2025-11-12 00:05:29 +0900

    [feature] PrimaryGame layout 클래스

---
+++ Source/KORAProject/UI/PrimaryLayout/KRPrimaryGameLayout.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommonUserWidget.h"
+#include "SubSystem/KRUIRouterSubsystem.h"
+#include "KRPrimaryGameLayout.generated.h"
+
+class UCommonActivatableWidgetStack;
+class UKRUIRouteTable;
+
+UCLASS(BlueprintType, Blueprintable)
+class KORAPROJECT_API UKRPrimaryGameLayout : public UCommonUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+    UCommonActivatableWidgetStack* GameLayerStack = nullptr;
+
+    UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+    UCommonActivatableWidgetStack* GameMenuLayerStack = nullptr;
+
+    UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+    UCommonActivatableWidgetStack* MenuLayerStack = nullptr;
+
+    UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+    UCommonActivatableWidgetStack* ModalLayerStack = nullptr;
+
+    UFUNCTION(BlueprintCallable, Category = "Layout")
+    UCommonActivatableWidgetStack* GetStack(EKRUILayer Layer) const
+    {
+        switch (Layer)
+        {
+        case EKRUILayer::Game:     return GameLayerStack;
+        case EKRUILayer::GameMenu: return GameMenuLayerStack;
+        case EKRUILayer::Menu:     return MenuLayerStack;
+        case EKRUILayer::Modal:    return ModalLayerStack;
+        default: return nullptr;
+        }
+    }
+
+protected:
+    virtual void NativeOnInitialized() override;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Layout")
+    TSoftObjectPtr<UKRUIRouteTable> RouteTable;
+};
