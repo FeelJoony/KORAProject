dd56bd93f45c345ccd4a1e876bb996870adc5e7f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-16 21:34:41 +0900

    KREnemyAttributeSet 속성 및 초기화 추가: 적 관련 속성(CurrentPoise, MaxPoise, PoiseRecoveryRate, PoiseRecoveryDelay) 및 초기화 함수 구현.

--- Source/KORAProject/GAS/AttributeSets/KREnemyAttributeSet.cpp
+++ Source/KORAProject/GAS/AttributeSets/KREnemyAttributeSet.cpp
@@ -1,5 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GAS/AttributeSets/KREnemyAttributeSet.h"
 
+UKREnemyAttributeSet::UKREnemyAttributeSet()
+{
+	InitCurrentPoise(1.f);
+	InitMaxPoise(1.f);
+	InitPoiseRecoveryRate(1.f);
+	InitPoiseRecoveryDelay(1.f);
+}
--- Source/KORAProject/GAS/AttributeSets/KREnemyAttributeSet.h
+++ Source/KORAProject/GAS/AttributeSets/KREnemyAttributeSet.h
@@ -1,17 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GAS/KRAbilitySystemComponent.h"
 #include "KREnemyAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class KORAPROJECT_API UKREnemyAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UKREnemyAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Enemy")
+	FGameplayAttributeData CurrentPoise;
+	ATTRIBUTE_ACCESSORS(UKREnemyAttributeSet, CurrentPoise)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Enemy")
+	FGameplayAttributeData MaxPoise;
+	ATTRIBUTE_ACCESSORS(UKREnemyAttributeSet, MaxPoise)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Enemy")
+	FGameplayAttributeData PoiseRecoveryRate;
+	ATTRIBUTE_ACCESSORS(UKREnemyAttributeSet, PoiseRecoveryRate)
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Enemy")
+	FGameplayAttributeData PoiseRecoveryDelay;
+	ATTRIBUTE_ACCESSORS(UKREnemyAttributeSet, PoiseRecoveryDelay)
 	
 };
