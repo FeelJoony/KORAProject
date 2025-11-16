f3ac8c36c06982571352db3af3216b01ecdc2e76
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-16 21:34:26 +0900

    KRCombatCommonSet 속성 및 초기화 추가: 전투 관련 속성(CurrentHealth, MaxHealth, AttackPower 등) 및 초기화 함수 구현.

--- Source/KORAProject/GAS/AttributeSets/KRCombatCommonSet.cpp
+++ Source/KORAProject/GAS/AttributeSets/KRCombatCommonSet.cpp
@@ -1,5 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GAS/AttributeSets/KRCombatCommonSet.h"
 
+UKRCombatCommonSet::UKRCombatCommonSet()
+{
+	InitCurrentHealth(100.f);
+	InitMaxHealth(100.f);
+	InitAttackPower(1.f);
+	InitDefense(0.f);
+	InitDealDamageMult(1.f);
+	InitTakeDamageMult(1.f);
+	InitDamageTaken(0.f);
+}
--- Source/KORAProject/GAS/AttributeSets/KRCombatCommonSet.h
+++ Source/KORAProject/GAS/AttributeSets/KRCombatCommonSet.h
@@ -1,17 +1,49 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GAS/KRAbilitySystemComponent.h"
 #include "KRCombatCommonSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class KORAPROJECT_API UKRCombatCommonSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UKRCombatCommonSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData Defense;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, Defense)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData DealDamageMult;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, DealDamageMult)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData TakeDamageMult;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, TakeDamageMult)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes|Combat")
+	FGameplayAttributeData DamageTaken;
+	ATTRIBUTE_ACCESSORS(UKRCombatCommonSet, DamageTaken)
 };
