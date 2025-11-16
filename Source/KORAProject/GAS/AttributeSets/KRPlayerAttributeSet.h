6b745b031d6cb9fa4ca6a196c17b991a22e16aca
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-16 20:31:38 +0900

    KRPlayerAttributeSet 클래스 추가: 플레이어 속성 관리를 위한 새로운 Attribute Set 생성.

---
+++ Source/KORAProject/GAS/AttributeSets/KRPlayerAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "KRPlayerAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API UKRPlayerAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
