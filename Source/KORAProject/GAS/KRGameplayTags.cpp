c0ec421eb049cde32a555e2cbcb2d11b71064b7f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-20 15:51:34 +0900

    KRGameplayTags 업데이트: 새 Player, Event, SetByCaller 관련 태그 추가 및 헤더/소스 정리.

--- Source/KORAProject/GAS/KRGameplayTags.cpp
+++ Source/KORAProject/GAS/KRGameplayTags.cpp
@@ -28,8 +28,11 @@ UE_DEFINE_GAMEPLAY_TAG(KRTag_Player_Weapon_Gun,		"Player.Weapon.Gun");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Player_Can_Step,		"Player.Can.Step");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Player_Can_Jump,		"Player.Can.Jump");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Player_Action_Ladder,	"Player.Action.Ladder");
+UE_DEFINE_GAMEPLAY_TAG(KRTag_Player_Mode_UI,		"Player.Mode.UI");
+
 
 /* Event Tags */
+UE_DEFINE_GAMEPLAY_TAG(KRTag_Event_Combat_Hit,		"Event.Combat.Hit");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Event_Equip_Sword,		"Event.Equip.Sword");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Event_Unequip_Sword,	"Event.Unequip.Sword");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Event_Equip_Gun,		"Event.Equip.Gun");
@@ -46,9 +49,14 @@ UE_DEFINE_GAMEPLAY_TAG(KRTag_Ability_Attack_Charge_Sword,	"Ability.Attack.Charge
 /* Enemy Tags */
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Enemy_Weapon,	"Enemy.Weapon");
 
-/*Cooldown Tags*/
+/* Cooldown Tags */
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Cooldown_Ability,		"Cooldown.Ability");
 UE_DEFINE_GAMEPLAY_TAG(KRTag_Cooldown_Ability_Jump, "Cooldown.Ability.Jump");
+
+/* SetByCaller Tags */
+UE_DEFINE_GAMEPLAY_TAG(KRTag_SetByCaller_BaseDamage, "SetByCaller.BaseDamage");
+UE_DEFINE_GAMEPLAY_TAG(KRTag_SetByCaller_AttackType_Light, "SetByCaller.AttackType.Light");
+UE_DEFINE_GAMEPLAY_TAG(KRTag_SetByCaller_AttackType_Charge, "SetByCaller.AttackType.Charge");
 
 // ----- PoolTags -----
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Pool_AlwaysOn, "Pool.AlwaysOn", "상점 상시 품목");
