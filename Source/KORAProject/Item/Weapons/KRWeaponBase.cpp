44a03b43fc8d07ca79072f23c28627a9b00c83ec
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-20 15:51:55 +0900

    KRWeaponBase 기능 확장: 무기 데이터 처리 관련 함수 추가 및 타겟 상호작용 델리게이트 구현.

--- Source/KORAProject/Item/Weapons/KRWeaponBase.cpp
+++ Source/KORAProject/Item/Weapons/KRWeaponBase.cpp
@@ -1,4 +1,8 @@
 #include "Item/Weapons/KRWeaponBase.h"
+#include "Data/DataAssets/DataAsset_Weapon.h"
+#include "Data/DataAssets/DataAsset_HeroWeapon.h"
+
+#include "KORADebugHelper.h"
 
 
 AKRWeaponBase::AKRWeaponBase()
@@ -11,4 +15,55 @@ AKRWeaponBase::AKRWeaponBase()
 	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	WeaponMesh->SetCollisionProfileName(TEXT("NoCollision"));
 	WeaponMesh->SetGenerateOverlapEvents(false);
+}
+
+void AKRWeaponBase::AssignGrantAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandles)
+{
+	GrantedAbilitySpecHandles = InSpecHandles;
+}
+
+TArray<FGameplayAbilitySpecHandle> AKRWeaponBase::GetGrantedAbilitySpecHandles() const
+{
+	return GrantedAbilitySpecHandles;
+}
+
+FKRWeaponCommonData AKRWeaponBase::GetCommonData() const
+{
+	if (WeaponData)
+	{
+		return WeaponData->CommonData;
+	}
+
+	Debug::Print(TEXT("WeaponData is missing"), FColor::Red);
+	return FKRWeaponCommonData();
+}
+
+bool AKRWeaponBase::GetHeroData(FKRHeroWeaponData& OutHeroData) const
+{
+	OutHeroData = FKRHeroWeaponData();
+	
+	if (!WeaponData) 
+	{
+		return false;
+	}
+	
+	if (const UDataAsset_HeroWeapon* HeroDataAsset = Cast<const UDataAsset_HeroWeapon>(WeaponData))
+	{
+		OutHeroData = HeroDataAsset->HeroWeaponData;
+		return true;
+	}
+
+	return false; 
+}
+
+
+
+void AKRWeaponBase::ReportHit(AActor* HitActor, const FHitResult& HitResult)
+{
+	OnWeaponHitTarget.Broadcast(HitActor, HitResult);
+}
+
+void AKRWeaponBase::ReportPulled(AActor* HitActor, const FHitResult& HitResult)
+{
+	OnWeaponPulledFromTarget.Broadcast(HitActor, HitResult);
 }
