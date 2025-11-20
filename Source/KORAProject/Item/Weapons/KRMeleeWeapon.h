5c46597482d21b1f530d79a866c84488111366f1
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-20 15:51:41 +0900

    MeleeWeapon 기능 확장: CollisionBox 오버랩 처리 로직 추가 및 관련 함수 구현.

--- Source/KORAProject/Item/Weapons/KRMeleeWeapon.h
+++ Source/KORAProject/Item/Weapons/KRMeleeWeapon.h
@@ -18,6 +18,13 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapons")
 	TObjectPtr<UBoxComponent> WeaponCollisionBox;
 
+	UFUNCTION()
+	virtual void OnCollisionBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnCollisionBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	
 public:
 	FORCEINLINE UBoxComponent* GetWeaponCollisionBox() const {return WeaponCollisionBox;}
 };
