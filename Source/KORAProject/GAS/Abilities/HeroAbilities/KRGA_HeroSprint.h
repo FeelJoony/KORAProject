64e2c938e5518591bf301f00472ffdf3134b519e
n-fia <78014168+n-fia@users.noreply.github.com>
2025-11-23 16:08:35 +0900

    Sprint 로직 복원 (ABP, GA, Montage 수정)
    GrapleGA 클래스 생성

--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroSprint.h
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroSprint.h
@@ -12,6 +12,8 @@ class KORAPROJECT_API UKRGA_HeroSprint : public UKRHeroGameplayAbility
 	GENERATED_BODY()
 	
 public:
+	UKRGA_HeroSprint();
+	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility,	bool bWasCancelled) override;
 
@@ -20,30 +22,28 @@ public:
 	
 protected:
 	UFUNCTION()
-	void OnAllowStep();
-	UFUNCTION()
-	void OnBlockStep();
-	UFUNCTION()
-	void OnAllowJumpStep();
+	void OnSprint();
 	UFUNCTION()
 	void OnAbilityEnd();
+
+	UPROPERTY(EditDefaultsOnly, Category = KRAbility)
+	float SprintChargeTime;
+	UPROPERTY(EditDefaultsOnly, Category = KRAbility)
+	float SprintSpeed;
+	UPROPERTY(EditDefaultsOnly, Category = KRAbility)
+	float WalkSpeed;
 
 	UPROPERTY(EditDefaultsOnly, Category=KRAbility)
 	FGameplayTag StepAbilityTag;
 	UPROPERTY(EditDefaultsOnly, Category=KRAbility)
 	FGameplayTag JumpAbilityTag;
-
-	UPROPERTY(EditDefaultsOnly, Category=KRAbility)
-	FGameplayTag CooldownJumpTag;
-
-	
-	UPROPERTY(EditDefaultsOnly, Category=KRAbility)
-	FGameplayTag CanStepTag;
-	UPROPERTY(EditDefaultsOnly, Category=KRAbility)
-	FGameplayTag CanJumpTag;
 	
 	UPROPERTY()
 	TObjectPtr<UAbilityTask_WaitDelay> EndDelayTask;
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_WaitDelay> StartSprintTask;
+	
+	bool bSprintStarted;
 	
 	FTimerHandle TimerHandle;
 };
