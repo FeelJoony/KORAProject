76cda4727d58762dfd09a3ca63b4bdedc1a18ef7
Juyee Myeong <juyee.ming@gmail.com>
2025-11-24 23:10:09 +0900

    [feature] GameplayMessage 서브시스템 반영한 재화 관련 UI 패널 클래스 생성 및 위젯 Reparent

---
+++ Source/KORAProject/UI/Currency/KROnlyGearingPanel.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Currency/KROnlyGearingPanel.h"
+#include "CommonNumericTextBlock.h"
+#include "Components/HorizontalBox.h"
+
+void UKROnlyGearingPanel::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (UWorld* World = GetWorld())
+	{
+		UGameplayMessageSubsystem& Subsys = UGameplayMessageSubsystem::Get(World);
+		CurrencyListener = Subsys.RegisterListener(
+			FKRUIMessageTags::Currency(),
+			this,
+			&ThisClass::OnCurrencyMessageReceived
+		);
+	}
+}
+
+void UKROnlyGearingPanel::NativeDestruct()
+{
+	if (UWorld* World = GetWorld())
+	{
+		UGameplayMessageSubsystem& Subsys = UGameplayMessageSubsystem::Get(World);
+		Subsys.UnregisterListener(CurrencyListener);
+	}
+
+	Super::NativeDestruct();
+}
+
+void UKROnlyGearingPanel::OnCurrencyMessageReceived(FGameplayTag Channel, const FKRUIMessage_Currency& Message)
+{
+	UpdateGearingPanel(Message.CurrentGearing, Message.DeltaGearing);
+}
+
+void UKROnlyGearingPanel::UpdateGearingPanel(int32 InCurrentGearing, int32 InLostGearing)
+{
+	if (CurrentGearing)
+	{
+		CurrentGearing->SetCurrentValue(InCurrentGearing);
+	}
+
+	if (LostGearing)
+	{
+		LostGearing->SetCurrentValue(InLostGearing);
+	}
+
+	if (LostGearingSection)
+	{
+		if (InLostGearing > 0)
+		{
+			LostGearingSection->SetVisibility(ESlateVisibility::Visible);
+		}
+		else
+		{
+			LostGearingSection->SetVisibility(ESlateVisibility::Collapsed);
+		}
+	}
+}
