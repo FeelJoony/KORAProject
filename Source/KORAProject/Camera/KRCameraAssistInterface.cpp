bc36ccdf357e67bc2f6716f57955d3d54cf26365
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-27 08:36:59 +0900

    KORAProject 업데이트: KRCameraAssistInterface 추가, 카메라 충돌 처리 관련 함수 인터페이스 정의.

---
+++ Source/KORAProject/Camera/KRCameraAssistInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Camera/KRCameraAssistInterface.h"
+
+// Add default functionality here for any IKRCameraAssistInterface functions that are not pure virtual.
