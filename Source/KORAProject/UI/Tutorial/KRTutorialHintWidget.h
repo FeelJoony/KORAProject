1fd6c27476c00c8f620ad739208069a130b2aacc
Juyee Myeong <juyee.ming@gmail.com>
2025-11-27 01:30:06 +0900

    [feature] 튜토리얼 위젯 클래스 생성해서 UI Message 구독해서 위젯 관리 구현

---
+++ Source/KORAProject/UI/Tutorial/KRTutorialHintWidget.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CommonActivatableWidget.h"
+#include "Data/TutorialDataStruct.h"
+#include "UI/Data/UIStruct/KRUIMessagePayloads.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "KRTutorialHintWidget.generated.h"
+
+class UCommonRichTextBlock;
+
+UCLASS()
+class KORAPROJECT_API UKRTutorialHintWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+public:
+    virtual void NativeOnInitialized() override;
+    virtual void NativeDestruct() override;
+
+    void NotifyStepCompleted(FName StepId);
+
+protected:
+    UPROPERTY(meta = (BindWidget)) class UCommonTextBlock* TutorialText;
+    UPROPERTY(meta = (BindWidgetOptional)) UWidget* TutorialContainer;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tutorial") UDataTable* TutorialPopupTable;
+
+    FTutorialDataStruct CurrentRow;
+    bool bHasActiveRow = false;
+
+    FTimerHandle DurationHandle;
+    bool bPausedByMe = false;
+
+    FGameplayMessageListenerHandle TutorialListener;
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Tutorial")
+    void BP_OnTutorialShown();
+
+    void HandleTutorialMessage(FGameplayTag Channel, const FKRUIMessage_Tutorial& Msg);
+    void ApplyRow(const FTutorialDataStruct& Row);
+    void ClosePopup();
+
+    void OnDurationTimeout();
+
+    bool IsTimedPopup(const FTutorialDataStruct& Row) const;
+    bool IsInputPopup(const FTutorialDataStruct& Row) const;
+    void ClearTimerAndUnpause();
+};
