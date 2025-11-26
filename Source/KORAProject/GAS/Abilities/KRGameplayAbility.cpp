4eabfa9502c63c55c9989c072121b1d16cd34f50
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-26 20:54:59 +0900

    KORAProject 업데이트: KRGameplayAbility 클래스 확장, Ability 활성화 정책 추가 및 ActorInfo 기반 유틸리티 함수 구현.

--- Source/KORAProject/GAS/Abilities/KRGameplayAbility.cpp
+++ Source/KORAProject/GAS/Abilities/KRGameplayAbility.cpp
@@ -1,32 +1,41 @@
 #include "GAS/Abilities/KRGameplayAbility.h"
+
+#include "Characters/KRBaseCharacter.h"
 #include "Components/Combat/PawnCombatComponent.h"
 #include "GAS/KRAbilitySystemComponent.h"
+#include "Player/KRPlayerController.h"
 
-void UKRGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+UKRGameplayAbility::UKRGameplayAbility(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
 {
-	Super::OnGiveAbility(ActorInfo, Spec);
-
-	if (AbilityActivationPolicy == EKRAbilityActivationPolicy::OnGiven)
-	{
-		if (ActorInfo && !Spec.IsActive())
-		{
-			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
-		}
-	}
+	ActivationPolicy = EKRAbilityActivationPolicy::OnInputTriggered;
 }
 
-void UKRGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+UKRAbilitySystemComponent* UKRGameplayAbility::GetKRAbilitySystemComponentFromActorInfo() const
 {
-	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	return (CurrentActorInfo ? Cast<UKRAbilitySystemComponent>(CurrentActorInfo->AbilitySystemComponent.Get()) : nullptr);
+}
 
-	if (AbilityActivationPolicy == EKRAbilityActivationPolicy::OnGiven)
+AKRPlayerController* UKRGameplayAbility::GetKRPlayerControllerFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<AKRPlayerController>(CurrentActorInfo->PlayerController.Get()) : nullptr);
+}
+
+AController* UKRGameplayAbility::GetControllerFromActorInfo() const
+{
+	if (CurrentActorInfo)
 	{
-		if (ActorInfo)
+		if (AController* PC = CurrentActorInfo->PlayerController.Get())
 		{
-			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+			return PC;
 		}
 	}
+	return nullptr;
+}
+
+AKRBaseCharacter* UKRGameplayAbility::GetKRCharacterFromActorInfo() const
+{
+	return (CurrentActorInfo ? Cast<AKRBaseCharacter>(CurrentActorInfo->AvatarActor.Get()) : nullptr);
 }
 
 UPawnCombatComponent* UKRGameplayAbility::GetPawnCombatComponentFromActorInfo() const
@@ -34,7 +43,38 @@ UPawnCombatComponent* UKRGameplayAbility::GetPawnCombatComponentFromActorInfo() 
 	return GetAvatarActorFromActorInfo()->FindComponentByClass<UPawnCombatComponent>();
 }
 
-UKRAbilitySystemComponent* UKRGameplayAbility::GetKRAbilitySystemComponentFromActorInfo() const
+void UKRGameplayAbility::TryActivateAbilityOnSpawn(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec) const
 {
-	return Cast<UKRAbilitySystemComponent>(CurrentActorInfo->AbilitySystemComponent);
+	if (ActorInfo && !Spec.IsActive() && (ActivationPolicy == EKRAbilityActivationPolicy::OnSpawn))
+	{
+		UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+		const AActor* AvatarActor = ActorInfo->AvatarActor.Get();
+
+		if (ASC && AvatarActor && !AvatarActor->GetTearOff() && (AvatarActor->GetLifeSpan() <= 0.f))
+		{
+			const bool bIsLocalExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalPredicted) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalOnly);
+			const bool bIsServerExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerOnly) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerInitiated);
+
+			const bool bClientShouldActivate = ActorInfo->IsLocallyControlled() && bIsLocalExecution;
+			const bool bServerShouldActivate = ActorInfo->IsNetAuthority() && bIsServerExecution;
+
+			if (bClientShouldActivate || bServerShouldActivate)
+			{
+				ASC->TryActivateAbility(Spec.Handle);
+			}
+		}
+	}
+}
+
+void UKRGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	
+}
+
+void UKRGameplayAbility::OnPawnAvatarSet()
+{
+	K2_OnPawnAvatarSet();
 }
