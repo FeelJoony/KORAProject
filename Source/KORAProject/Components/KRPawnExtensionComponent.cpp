dcb88c16e8d768fabae1582a70ec90aa7f59b9bb
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-26 20:53:42 +0900

    KORAProject 업데이트: KRPawnExtensionComponent 클래스 추가, Pawn 데이터와 AbilitySystem 초기화 및 상태 관리 로직 구현.

---
+++ Source/KORAProject/Components/KRPawnExtensionComponent.cpp
@@ -0,0 +1,229 @@
+#include "Components/KRPawnExtensionComponent.h"
+#include "GAS/KRAbilitySystemComponent.h"
+#include "Data/DataAssets/KRPawnData.h"
+#include "Components/GameFrameworkComponentManager.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/Pawn.h"
+#include "GameplayTag/KRStateTag.h"
+#include "Net/UnrealNetwork.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(KRPawnExtensionComponent)
+
+const FName UKRPawnExtensionComponent::Name_ActorFeatureName("PawnExtension");
+
+UKRPawnExtensionComponent::UKRPawnExtensionComponent(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	SetIsReplicatedByDefault(true);
+
+	PawnData = nullptr;
+	AbilitySystemComponent = nullptr;
+}
+
+void UKRPawnExtensionComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(UKRPawnExtensionComponent, PawnData);
+}
+
+bool UKRPawnExtensionComponent::CanChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState,
+	FGameplayTag DesiredState) const
+{
+	check(Manager);
+
+	APawn* Pawn = GetPawn<APawn>();
+
+	if (!CurrentState.IsValid() && DesiredState == KRTAG_STATE_INIT_SPAWNED)
+	{
+		if (Pawn) return true;
+	}
+
+	if (CurrentState == KRTAG_STATE_INIT_SPAWNED && DesiredState == KRTAG_STATE_INIT_DATAAVAILABLE)
+	{
+		if (!PawnData) return false;
+
+		const bool bHasAuthority = Pawn->HasAuthority();
+		const bool bIsLocallyControlled = Pawn->IsLocallyControlled();
+
+		if (bHasAuthority || bIsLocallyControlled)
+		{
+			if (!GetController<AController>()) return false;
+		}
+
+		return true;
+	}
+	else if (CurrentState == KRTAG_STATE_INIT_DATAAVAILABLE && DesiredState == KRTAG_STATE_INIT_DATAINITIALIZED)
+	{
+		return Manager->HaveAllFeaturesReachedInitState(Pawn, KRTAG_STATE_INIT_DATAAVAILABLE);
+	}
+	else if (CurrentState == KRTAG_STATE_INIT_DATAINITIALIZED && DesiredState == KRTAG_STATE_INIT_GAMEPLAYREADY)
+	{
+		return true;
+	}
+	
+	return false;
+}
+
+void UKRPawnExtensionComponent::HandleChangeInitState(UGameFrameworkComponentManager* Manager,
+	FGameplayTag CurrentState, FGameplayTag DesiredState)
+{
+	// 상태 변경 시 추가 로직 필요하면 작성
+}
+
+void UKRPawnExtensionComponent::OnActorInitStateChanged(const FActorInitStateChangedParams& Params)
+{
+	if (Params.FeatureName != Name_ActorFeatureName)
+	{
+		if (Params.FeatureState == KRTAG_STATE_INIT_DATAAVAILABLE)
+		{
+			CheckDefaultInitialization();
+		}
+	}
+}
+
+void UKRPawnExtensionComponent::CheckDefaultInitialization()
+{
+	CheckDefaultInitializationForImplementers();
+
+	static const TArray<FGameplayTag> StateChain = {
+		KRTAG_STATE_INIT_SPAWNED,
+		KRTAG_STATE_INIT_DATAAVAILABLE,
+		KRTAG_STATE_INIT_DATAINITIALIZED,
+		KRTAG_STATE_INIT_GAMEPLAYREADY
+	};
+
+	ContinueInitStateChain(StateChain);
+}
+
+void UKRPawnExtensionComponent::SetPawnData(const UKRPawnData* InPawnData)
+{
+	check(InPawnData);
+
+	APawn* Pawn = GetPawnChecked<APawn>();
+
+	if (Pawn->GetLocalRole() != ROLE_Authority)
+	{
+		return;
+	}
+
+	if (PawnData)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Trying to set PawnData [%s] on pawn [%s] that already has valid PawnData [%s]."), *GetNameSafe(InPawnData), *GetNameSafe(Pawn), *GetNameSafe(PawnData));
+		return;
+	}
+
+	PawnData = InPawnData;
+
+	Pawn->ForceNetUpdate();
+
+	CheckDefaultInitialization();
+}
+
+void UKRPawnExtensionComponent::InitializeAbilitySystem(UKRAbilitySystemComponent* InASC, AActor* InOwnerActor)
+{
+	check(InASC);
+	check(InOwnerActor);
+
+	if (AbilitySystemComponent == InASC)
+	{
+		return;
+	}
+
+	if (AbilitySystemComponent)
+	{
+		UninitializeAbilitySystem();
+	}
+
+	APawn* Pawn = GetPawnChecked<APawn>();
+	AActor* ExistingAvatar = InASC->GetAvatarActor();
+
+	if ((ExistingAvatar != nullptr) && (ExistingAvatar != Pawn))
+	{
+		if (UKRPawnExtensionComponent* OtherExtensionComponent = FindPawnExtensionComponent(ExistingAvatar))
+		{
+			OtherExtensionComponent->UninitializeAbilitySystem();
+		}
+	}
+
+	AbilitySystemComponent = InASC;
+	AbilitySystemComponent->InitAbilityActorInfo(InOwnerActor, Pawn);
+
+	if (PawnData)
+	{
+		InASC->SetTagRelationshipMapping(PawnData->TagRelationshipMapping);
+	}
+	OnAbilitySystemInitialized.Broadcast();
+}
+
+void UKRPawnExtensionComponent::UninitializeAbilitySystem()
+{
+	if (!AbilitySystemComponent)
+	{
+		return;
+	}
+
+	if (AbilitySystemComponent->GetAvatarActor() == GetOwner())
+	{
+		AbilitySystemComponent->SetAvatarActor(nullptr);
+	}
+	
+	AbilitySystemComponent = nullptr;
+	OnAbilitySystemUninitialized.Broadcast();
+}
+
+void UKRPawnExtensionComponent::HandleControllerChanged()
+{
+	if (AbilitySystemComponent && (AbilitySystemComponent->GetAvatarActor() == GetPawnChecked<APawn>()))
+	{
+		AbilitySystemComponent->RefreshAbilityActorInfo();
+	}
+
+	CheckDefaultInitialization();
+}
+
+void UKRPawnExtensionComponent::HandlePlayerStateReplicated()
+{
+	CheckDefaultInitialization();
+}
+
+void UKRPawnExtensionComponent::SetupPlayerInputComponent()
+{
+	CheckDefaultInitialization();
+}
+
+void UKRPawnExtensionComponent::OnRegister()
+{
+	Super::OnRegister();
+
+	const APawn* Pawn = GetPawn<APawn>();
+	ensureAlwaysMsgf((Pawn != nullptr), TEXT("KRPawnExtensionComponent on [%s] can only be added to Pawn actors."), *GetNameSafe(GetOwner()));
+
+	RegisterInitStateFeature();
+}
+
+void UKRPawnExtensionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BindOnActorInitStateChanged(NAME_None, FGameplayTag(), false);
+
+	ensure(TryToChangeInitState(KRTAG_STATE_INIT_SPAWNED));
+	CheckDefaultInitialization();
+}
+
+void UKRPawnExtensionComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UninitializeAbilitySystem();
+	UnregisterInitStateFeature();
+	
+	Super::EndPlay(EndPlayReason);
+}
+
+void UKRPawnExtensionComponent::OnRep_PawnData()
+{
+	CheckDefaultInitialization();
+}
