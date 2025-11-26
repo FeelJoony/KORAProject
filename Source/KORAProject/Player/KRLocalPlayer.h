e168f53a1356252a56ffb58d5a2d0a3d1c70bab3
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-26 20:53:54 +0900

    KORAProject 업데이트: KRLocalPlayer 클래스 추가, UCommonLocalPlayer 상속 및 기본 구조 설정.

---
+++ Source/KORAProject/Player/KRLocalPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonLocalPlayer.h"
+#include "KRLocalPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API UKRLocalPlayer : public UCommonLocalPlayer
+{
+	GENERATED_BODY()
+	
+};
