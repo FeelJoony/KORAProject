97efa08cab11a3f7765af92b1406f836d7fed074
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-27 01:12:42 +0900

    KORAProject 업데이트: KRPlayerController 클래스 확장, AbilitySystemComponent 처리 및 KRPlayerState 참조 함수 추가.

--- Source/KORAProject/Player/KRPlayerController.cpp
+++ Source/KORAProject/Player/KRPlayerController.cpp
@@ -1,5 +1,44 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Player/KRPlayerController.h"
+#include "Player/KRPlayerState.h"
+#include "GAS/KRAbilitySystemComponent.h"
 
+AKRPlayerController::AKRPlayerController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void AKRPlayerController::OnUnPossess()
+{
+	if (APawn* PawnUnPossess = GetPawn())
+	{
+		if (UKRAbilitySystemComponent* KRASC = GetKRAbilitySystemComponent())
+		{
+			if (KRASC->GetAvatarActor() == PawnUnPossess)
+			{
+				KRASC->SetAvatarActor(nullptr);
+			}
+		}
+	}
+	Super::OnUnPossess();
+}
+
+void AKRPlayerController::PostProcessInput(const float DeltaTime, const bool bGamePaused)
+{
+	if (UKRAbilitySystemComponent* KRASC = GetKRAbilitySystemComponent())
+	{
+		KRASC->ProcessAbilityInput(DeltaTime, bGamePaused);
+	}
+
+	Super::PostProcessInput(DeltaTime, bGamePaused);
+}
+
+AKRPlayerState* AKRPlayerController::GetKRPlayerState() const
+{
+	return CastChecked<AKRPlayerState>(PlayerState, ECastCheckedType::NullAllowed);
+}
+
+UKRAbilitySystemComponent* AKRPlayerController::GetKRAbilitySystemComponent() const
+{
+	const AKRPlayerState* KRPS = GetKRPlayerState();
+	return (KRPS ? KRPS->GetKRAbilitySystemComponent() : nullptr);
+}
--- Source/KORAProject/Player/KRPlayerController.h
+++ Source/KORAProject/Player/KRPlayerController.h
@@ -4,10 +4,24 @@
 #include "CommonPlayerController.h"
 #include "KRPlayerController.generated.h"
 
+class AKRPlayerState;
+class UKRAbilitySystemComponent;
 
 UCLASS()
 class KORAPROJECT_API AKRPlayerController : public ACommonPlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AKRPlayerController(const FObjectInitializer& ObjectInitializer);
+
+	virtual void OnUnPossess() override;
 	
+	virtual void PostProcessInput(const float DeltaTime, const bool bGamePaused) override;
+
+	UFUNCTION(BlueprintCallable, Category = "KR|PlayerController")
+	AKRPlayerState* GetKRPlayerState() const;
+
+	UFUNCTION(BlueprintCallable, Category = "KR|PlayerController")
+	UKRAbilitySystemComponent* GetKRAbilitySystemComponent() const;
 };
