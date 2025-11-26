a353408f178b475a279b986b8482e0942bbecf25
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-11-26 20:53:34 +0900

    KORAProject 업데이트: KRPlayerController 클래스 생성, ACommonPlayerController 상속 및 기본 구조 설정.

---
+++ Source/KORAProject/Player/KRPlayerController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonPlayerController.h"
+#include "KRPlayerController.generated.h"
+
+
+UCLASS()
+class KORAPROJECT_API AKRPlayerController : public ACommonPlayerController
+{
+	GENERATED_BODY()
+	
+};
