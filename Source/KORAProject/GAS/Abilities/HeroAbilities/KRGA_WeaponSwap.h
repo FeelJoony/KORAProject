69b25599ca6ac074fc8e17368fbee2197305b295
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-02 13:28:23 +0900

    add: KRGA_WeaponSwap 능력 클래스 추가

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_WeaponSwap.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/KRGameplayAbility.h"
+#include "KRGA_WeaponSwap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API UKRGA_WeaponSwap : public UKRGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
