8299ba3d1a7034ea802da2532e7d8fd71cb7b828
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-02 13:28:11 +0900

    add: KRGA_EnterCombat 능력 클래스 추가

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_EnterCombat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/KRGameplayAbility.h"
+#include "KRGA_EnterCombat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API UKRGA_EnterCombat : public UKRGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
