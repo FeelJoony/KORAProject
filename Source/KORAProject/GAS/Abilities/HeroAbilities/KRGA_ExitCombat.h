0d87bf8ed16e7424153ca9e0106083ea36252460
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-02 13:28:15 +0900

    add: KRGA_ExitCombat 능력 클래스 추가

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_ExitCombat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/KRGameplayAbility.h"
+#include "KRGA_ExitCombat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KORAPROJECT_API UKRGA_ExitCombat : public UKRGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
