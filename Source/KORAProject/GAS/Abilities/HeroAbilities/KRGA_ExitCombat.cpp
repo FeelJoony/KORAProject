b2fdd602e7200e0a4b04952b521b9871c97eb7ef
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-03 22:42:45 +0900

    "update: 전투 종료 능력에 무기 관리 및 태그 처리 로직 추가"

--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_ExitCombat.cpp
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_ExitCombat.cpp
@@ -1,5 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GAS/Abilities/HeroAbilities/KRGA_ExitCombat.h"
+#include "Equipment/KREquipmentManagerComponent.h"
+#include "Weapons/KRWeaponInstance.h"
+#include "GAS/KRAbilitySystemComponent.h"
+#include "Characters/KRBaseCharacter.h"
+#include "Player/KRPlayerController.h"
+#include "GameplayTag/KRPlayerTag.h"
 
+void UKRGA_ExitCombat::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	AKRBaseCharacter* Character = Cast<AKRBaseCharacter>(ActorInfo->AvatarActor.Get());
+	AKRPlayerController* PC = Cast<AKRPlayerController>(ActorInfo->PlayerController.Get());
+	UKREquipmentManagerComponent* EquipComp = Character ? Character->FindComponentByClass<UKREquipmentManagerComponent>() : nullptr;
+	UKRAbilitySystemComponent* KRASC = GetKRAbilitySystemComponentFromActorInfo();
+
+	if (!Character || !PC || !EquipComp || !KRASC)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	TArray<UKREquipmentInstance*> AllWeapons = EquipComp->GetEquipmentInstancesOfType(UKRWeaponInstance::StaticClass());
+
+	for (UKREquipmentInstance* Equip : AllWeapons)
+	{
+		if (UKRWeaponInstance* Weapon = Cast<UKRWeaponInstance>(Equip))
+		{
+			Weapon->RemoveWeaponAbilities(KRASC);
+			Weapon->RemoveWeaponInputContext(PC);
+			Weapon->SetWeaponActiveState(false);
+		}
+	}
+
+	Character->GetMesh()->LinkAnimClassLayers(nullptr);
+	KRASC->RemoveLooseGameplayTag(KRTAG_PLAYER_MODE_SWORD);
+	KRASC->RemoveLooseGameplayTag(KRTAG_PLAYER_MODE_GUN);
+	KRASC->AddLooseGameplayTag(KRTAG_PLAYER_MODE_BASE);
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
