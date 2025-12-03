09276fda1606e21054a84489acab8f94908ab2ed
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-03 22:42:45 +0900

    "update: 전투 종료 능력에 무기 관리 및 태그 처리 로직 추가"

--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_ExitCombat.h
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_ExitCombat.h
@@ -1,17 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GAS/Abilities/KRGameplayAbility.h"
 #include "KRGA_ExitCombat.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class KORAPROJECT_API UKRGA_ExitCombat : public UKRGameplayAbility
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 };
