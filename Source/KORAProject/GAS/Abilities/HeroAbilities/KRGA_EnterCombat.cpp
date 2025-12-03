3660d9a878acb8711cfa0b936d0743761d6c3224
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-03 22:42:39 +0900

    "update: 전투 입장 능력 로직 및 무기 관리 기능 추가"

--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_EnterCombat.cpp
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_EnterCombat.cpp
@@ -1,5 +1,78 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GAS/Abilities/HeroAbilities/KRGA_EnterCombat.h"
+#include "Equipment/KREquipmentManagerComponent.h"
+#include "Weapons/KRWeaponInstance.h"
+#include "GAS/KRAbilitySystemComponent.h"
+#include "Characters/KRBaseCharacter.h"
+#include "Player/KRPlayerController.h"
+#include "GameplayTag/KRPlayerTag.h"
 
+void UKRGA_EnterCombat::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	AKRBaseCharacter* Character = Cast<AKRBaseCharacter>(ActorInfo->AvatarActor.Get());
+	AKRPlayerController* PC = Cast<AKRPlayerController>(ActorInfo->PlayerController.Get());
+	UKREquipmentManagerComponent* EquipComp = Character ? Character->FindComponentByClass<UKREquipmentManagerComponent>() : nullptr;
+	UKRAbilitySystemComponent* KRASC = GetKRAbilitySystemComponentFromActorInfo();
+
+	if (!Character || !PC || !EquipComp || !KRASC)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	FGameplayTag SwordTag = FGameplayTag::RequestGameplayTag("ItemType.Equip.Sword");
+	FGameplayTag GunTag = FGameplayTag::RequestGameplayTag("ItemType.Equip.Gun");
+
+	UKRWeaponInstance* SwordInst = nullptr;
+	UKRWeaponInstance* GunInst = nullptr;
+
+	TArray<UKREquipmentInstance*> AllWeapons = EquipComp->GetEquipmentInstancesOfType(UKRWeaponInstance::StaticClass());
+	for (UKREquipmentInstance* Equip : AllWeapons)
+	{
+		if (UKRWeaponInstance* Weapon = Cast<UKRWeaponInstance>(Equip))
+		{
+			if (Weapon->WeaponType.MatchesTag(SwordTag))
+			{
+				SwordInst = Weapon;
+			}
+			else if (Weapon->WeaponType.MatchesTag(GunTag))
+			{
+				GunInst = Weapon;
+			}
+		}
+	}
+
+	if (SwordInst)
+	{
+		SwordInst->SetWeaponActiveState(true);
+	}
+	if (GunInst)
+	{
+		GunInst->SetWeaponActiveState(true);
+	}
+
+	if (SwordInst)
+	{
+		SwordInst->GrantWeaponAbilities(KRASC);
+		SwordInst->ApplyWeaponAnimLayer(Character);
+		SwordInst->AddWeaponInputContext(PC);
+	}
+
+	if (GunInst)
+	{
+		GunInst->RemoveWeaponAbilities(KRASC);
+		GunInst->RemoveWeaponInputContext(PC);
+	}
+	
+	KRASC->RemoveLooseGameplayTag(KRTAG_PLAYER_MODE_BASE);
+	KRASC->AddLooseGameplayTag(KRTAG_PLAYER_MODE_SWORD);
+	
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
