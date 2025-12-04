b6fc19f919efbc3c595fcdcbeb78a7b90b33914b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-04 16:06:10 +0900

    "update: 근접 공격 능력 기본 클래스 추가"

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_MeleeBase.cpp
@@ -0,0 +1,74 @@
+﻿#include "KRGameplayAbility_MeleeBase.h"
+#include "Characters/KRBaseCharacter.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "GameplayTag/KREventTag.h"
+#include "GameplayTag/KRSetByCallerTag.h"
+#include "AbilitySystemBlueprintLibrary.h"
+
+UKRGameplayAbility_MeleeBase::UKRGameplayAbility_MeleeBase(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UKRGameplayAbility_MeleeBase::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	ListenForHitEvent();
+}
+
+void UKRGameplayAbility_MeleeBase::ListenForHitEvent()
+{
+	FGameplayTag HitTag = KRTAG_EVENT_COMBAT_HIT;
+
+	UAbilityTask_WaitGameplayEvent* Task = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+		this,
+		HitTag,
+		nullptr,
+		false,
+		false
+		);
+
+	if (Task)
+	{
+		Task->EventReceived.AddDynamic(this, &ThisClass::OnTargetHit);
+		Task->ReadyForActivation();
+	}
+}
+
+void UKRGameplayAbility_MeleeBase::OnTargetHit(FGameplayEventData Payload)
+{
+	AActor* TargetActor = const_cast<AActor*>(Payload.Target.Get());
+	if (!TargetActor) return;
+
+	float WeaponBaseDamage = Payload.EventMagnitude;
+	float FinalDamage = WeaponBaseDamage * CurrentDamageMultiplier;
+
+	if (DamageEffectClass)
+	{
+		FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, GetAbilityLevel());
+		if (SpecHandle.IsValid())
+		{
+			SpecHandle.Data->SetSetByCallerMagnitude(
+				KRTAG_SETBYCALLER_BASEDAMAGE,
+				FinalDamage
+				);
+
+			if (Payload.ContextHandle.GetHitResult())
+			{
+				SpecHandle.Data->GetContext().AddHitResult(*Payload.ContextHandle.GetHitResult());
+			}
+
+			FGameplayAbilityTargetDataHandle TargetData = UAbilitySystemBlueprintLibrary::AbilityTargetDataFromActor(TargetActor);
+
+			TArray<FActiveGameplayEffectHandle> AppliedHandles = ApplyGameplayEffectSpecToTarget(
+				CurrentSpecHandle,
+				CurrentActorInfo,
+				CurrentActivationInfo,
+				SpecHandle,
+				TargetData
+				);
+		}
+	}
+}
---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_MeleeBase.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "KRGameplayAbility_AttackBase.h"
+#include "KRGameplayAbility_MeleeBase.generated.h"
+
+UCLASS()
+class KORAPROJECT_API UKRGameplayAbility_MeleeBase : public UKRGameplayAbility_AttackBase
+{
+	GENERATED_BODY()
+
+public:
+	UKRGameplayAbility_MeleeBase(const FObjectInitializer& ObjectInitializer);
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+
+	float CurrentDamageMultiplier = 1.f;
+
+protected:
+	void ListenForHitEvent();
+
+	UFUNCTION()
+	void OnTargetHit(FGameplayEventData Payload);
+};
