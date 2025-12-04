7f2bf1a2391fa65013f126f1d55ce39faed1022f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-04 19:23:54 +0900

    "update: Tick 기반 능력 태스크 클래스 추가"

---
+++ Source/KORAProject/GAS/Abilities/Tasks/KRAbilityTask_WaitTick.cpp
@@ -0,0 +1,28 @@
+﻿#include "KRAbilityTask_WaitTick.h"
+
+UKRAbilityTask_WaitTick::UKRAbilityTask_WaitTick(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bTickingTask = true;
+}
+
+UKRAbilityTask_WaitTick* UKRAbilityTask_WaitTick::WaitTick(UGameplayAbility* OwningAbility, FName TaskInstanceName)
+{
+	UKRAbilityTask_WaitTick* MyObj = NewAbilityTask<UKRAbilityTask_WaitTick>(OwningAbility, TaskInstanceName);
+	return MyObj;
+}
+
+void UKRAbilityTask_WaitTick::Activate()
+{
+	Super::Activate();
+}
+
+void UKRAbilityTask_WaitTick::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnTick.Broadcast(DeltaTime);
+	}
+}
