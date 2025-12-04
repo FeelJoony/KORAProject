ba165bdab4011f8afbf4bfb2c115e5084fa9e48e
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-04 16:06:55 +0900

    "update: 경량 공격 능력 클래스 추가 및 콤보 처리 구현"

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_LightAttack.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "KRGameplayAbility_MeleeBase.h"
+#include "KRGameplayAbility_LightAttack.generated.h"
+
+UCLASS()
+class KORAPROJECT_API UKRGameplayAbility_LightAttack : public UKRGameplayAbility_MeleeBase
+{
+	GENERATED_BODY()
+
+public:
+	UKRGameplayAbility_LightAttack(const FObjectInitializer& ObjectInitializer);
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMontageInterrupted();
+
+};
