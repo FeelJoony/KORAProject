a39f5614180e17fa24a26a8df43c2c1b507fd4fd
Juyee Myeong <juyee.ming@gmail.com>
2025-12-04 14:14:52 +0900

    [feature] ProgressBar 스탯 관련된 부분은 타이머 기반으로 ASC를 바인딩하여 AttributeSet의 변화를 관찰하며 변동할 수 있도록 수정 / 가드 관련 메세지 구독

--- Source/KORAProject/UI/HUD/Modules/KRPlayerStatusWidget.h
+++ Source/KORAProject/UI/HUD/Modules/KRPlayerStatusWidget.h
@@ -8,9 +8,13 @@
 #include "KRPlayerStatusWidget.generated.h"
 
 class UProgressBar;
-class UKRAttributeSet;
-class UImage;
-class UTextBlock;
+class UWidget;
+class UCommonTextBlock;
+
+class UKRCombatCommonSet;
+class UKRPlayerAttributeSet;
+class UAbilitySystemComponent;
+struct FOnAttributeChangeData;
 
 UCLASS()
 class KORAPROJECT_API UKRPlayerStatusWidget : public UKRHUDWidgetBase
@@ -28,27 +32,60 @@ protected:
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UProgressBar> GreyHPBar = nullptr;
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UProgressBar> StaminaBar = nullptr;
 
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UWidget> GunAmmoSection = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UCommonTextBlock> CurrentBullet;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UCommonTextBlock> MaxBullet;
+
+	void BindToASC();
+	void InitBarsFromASC();
+
 	UFUNCTION(BlueprintImplementableEvent, Category = "ProgressBar")
 	void BP_OnCoreDriveChanged(int32 FullSegments, float PartialSegmentPercent);
 	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus", meta = (DisplayName = "On Weapon Changed"))
-	void BP_OnWeaponChanged(const FKRUIMessage_Weapon& WeaponData);
+	void BP_OnWeaponChanged(const FGameplayTag& WeaponTypeTag);
+	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus")
+	void BP_OnGuardSuccess();
+	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus")
+	void BP_OnPerfectGuardSuccess();
+	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus")
+	void BP_OnStaminaDepleted();
+	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus")
+	void BP_OnCoreDriveSegmentFilled(int32 NewFullSegments);
+	UFUNCTION(BlueprintImplementableEvent, Category = "PlayerStatus")
+	void BP_OnCoreDriveFull();
 
-	void OnProgressMessageReceived(FGameplayTag Channel, const FKRUIMessage_Progress& Message);
-	void OnGreyHPMessageReceived(FGameplayTag Channel, const FKRUIMessage_GreyHP& Message);
+	void OnHealthAttributeChanged(const FOnAttributeChangeData& Data);
+	void OnGreyHealthAttributeChanged(const FOnAttributeChangeData& Data);
+	void OnStaminaAttributeChanged(const FOnAttributeChangeData& Data);
+	void OnCoreDriveAttributeChanged(const FOnAttributeChangeData& Data);
+
 	void OnWeaponMessageReceived(FGameplayTag Channel, const FKRUIMessage_Weapon& Message);
-
-	UPROPERTY(EditDefaultsOnly, Category = "PlayerStatus|GreyHP") float GreyHPDecayDuration = 1.0f;
+	void OnGuardMessageReceived(FGameplayTag Channel, const FKRUIMessage_Guard& Message);
 
 private:
-	FGameplayMessageListenerHandle ProgressMessageHandle;
-	FGameplayMessageListenerHandle GreyHPMessageHandle;
+	TWeakObjectPtr<UAbilitySystemComponent>     CachedASC;
+	TWeakObjectPtr<const UKRCombatCommonSet>    CombatAttr;   // CurrentHealth / MaxHealth
+	TWeakObjectPtr<const UKRPlayerAttributeSet> PlayerAttr;	// Stamina / CoreDrive
+
+	FDelegateHandle HealthChangedHandle;
+	FDelegateHandle GreyHPChangedHandle;
+	FDelegateHandle StaminaChangedHandle;
+	FDelegateHandle CoreDriveChangedHandle;
+
 	FGameplayMessageListenerHandle WeaponMessageHandle;
+	FGameplayMessageListenerHandle GuardMessageHandle;
 
 	float HealthDisplayPercent = 1.f;
 	float HealthTargetPercent = 1.f;
 	float HealthAnimTime = 0.3f;
 	float HealthAnimElapsed = 0.f;
 	FTimerHandle HealthAnimTimerHandle;
+
+	float GreyHPDisplayPercent = 1.f;
+	float GreyHPTargetPercent = 1.f;
+	float GreyHPAnimTime = 0.4f;
+	float GreyHPAnimElapsed = 0.f;
+	FTimerHandle GreyHPAnimTimerHandle;
 
 	float StaminaDisplayPercent = 1.f;
 	float StaminaTargetPercent = 1.f;
@@ -59,23 +96,20 @@ private:
 	float CoreDriveValue = 0.f;
 	float CoreDriveMax = 60.f;
 
-	float CurrentGreyHP = 0.f;
-	float GreyHPDecayStartValue = 0.f;
-	float GreyHPDecayElapsed = 0.f;
-	FTimerHandle GreyHPDecayTimerHandle;
-
+	int32 CoreDriveFullSegmentsPrev = 0;
+	bool bIsCoreDriveFull = false;
 
 	bool IsMessageFromMyActor(const TWeakObjectPtr<AActor>& TargetActor) const;
 
 	void StartHealthAnim();
 	void TickHealthAnim();
 
+	void StartGreyHPAnim();
+	void TickGreyHPAnim();
+
 	void StartStaminaAnim();
 	void TickStaminaAnim();
 
-	void RecoverGreyHP(float RecoveredAmount);
-	void DecayGreyHP();
-	void TickGreyHPDecayAnimation();
-
 	void UpdateGreyHPBar(float NewValueNormalized);
+	void UpdateWeaponAmmoDisplay(const FGameplayTag& WeaponTypeTag, int32 CurrentAmmo, int32 MaxAmmo);
 };
