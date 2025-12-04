9bf11f742e71bdba7696b587172a8315dbe35323
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-04 16:04:35 +0900

    "update: 무기 공격 애니메이션 및 차지 로직 추가"

--- Source/KORAProject/Weapons/KRWeaponInstance.cpp
+++ Source/KORAProject/Weapons/KRWeaponInstance.cpp
@@ -117,6 +117,8 @@ void UKRWeaponInstance::InitializeFromItem(UKRInventoryItemInstance* ItemInstanc
         CachedIMC = EquipFragment->WeaponIMC;
         CachedIMCPriority = EquipFragment->InputPriority;
         CachedAbilities = EquipFragment->GrantedAbilities;
+        LightAttackMontages = EquipFragment->LightAttackMontages;
+        ChargeAttackMontages = EquipFragment->ChargeAttackMontages;
     }
 }
 
@@ -304,6 +306,29 @@ void UKRWeaponInstance::RemoveWeaponInputContext(AKRPlayerController* TargetPC)
             Subsystem->RemoveMappingContext(CachedIMC);
         }
     }
+}
+
+UAnimMontage* UKRWeaponInstance::GetLightAttackMontage(int32 ComboIndex) const
+{
+    if (LightAttackMontages.IsValidIndex(ComboIndex))
+    {
+        return LightAttackMontages[ComboIndex];
+    }
+    return nullptr;
+}
+
+UAnimMontage* UKRWeaponInstance::GetChargeAttackMontage(int32 ComboIndex) const
+{
+    if (ChargeAttackMontages.IsValidIndex(ComboIndex))
+    {
+        return ChargeAttackMontages[ComboIndex];
+    }
+    return nullptr;
+}
+
+float UKRWeaponInstance::GetWeaponChargeTime() const
+{
+    return ChargeTime;
 }
 
 void UKRWeaponInstance::GrantWeaponAbilities(UKRAbilitySystemComponent* ASC)
