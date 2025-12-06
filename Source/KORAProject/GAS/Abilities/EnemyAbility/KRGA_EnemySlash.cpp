1ef3166ff4f416bccf735cdcfad273cea56ebc89
LimHyeongGyun <200hk@naver.com>
2025-12-06 11:45:54 +0900

    [feature] Enemy Attack
    GA_Enemy_Slash가 발동하여 플레이어를 공격했을 때 KRTAG_EVENT_COMBAT_HIT 이벤트가 발생하도록
    UKRGameplayAbility_MeleeBase를 상속시켜 GA_Enemy_Slash 변경

--- Source/KORAProject/GAS/Abilities/EnemyAbility/KRGA_EnemySlash.cpp
+++ Source/KORAProject/GAS/Abilities/EnemyAbility/KRGA_EnemySlash.cpp
@@ -2,6 +2,7 @@
 #include "Enemy/KRHumanEnemyCharacter.h"
 #include "GameplayTag/KREnemyTag.h"
 #include "GAS/KRAbilitySystemComponent.h"
+#include "Components/KRCombatComponent.h"
 
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 
@@ -25,6 +26,18 @@ void UKRGA_EnemySlash::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		return;
 	}
 
+	UKRWeaponInstance* Weapon = nullptr;
+	if (UKRCombatComponent* CombatComp = GetCombatComponentFromActorInfo())
+	{
+		Weapon = CombatComp->GetCurrentWeaponInstance();
+	}
+
+	if (!Weapon)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
 	AKREnemyCharacter* Enemy = Cast<AKREnemyCharacter>(GetAvatarActorFromActorInfo());
 	if (!IsValid(Enemy))
 	{
@@ -42,7 +55,14 @@ void UKRGA_EnemySlash::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 				NAME_None
 			);
 
-		ActivateSlash();
+		if (MontageTask)
+		{
+			MontageTask->OnCompleted.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
+			MontageTask->OnCancelled.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
+			MontageTask->OnInterrupted.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
+			MontageTask->OnBlendOut.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
+			MontageTask->ReadyForActivation();
+		}
 	}
 }
 
@@ -61,15 +81,6 @@ void UKRGA_EnemySlash::EndAbility(const FGameplayAbilitySpecHandle Handle,
 	}
 
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
-}
-
-void UKRGA_EnemySlash::ActivateSlash()
-{
-	MontageTask->OnCompleted.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
-	MontageTask->OnCancelled.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
-	MontageTask->OnInterrupted.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
-	MontageTask->OnBlendOut.AddDynamic(this, &UKRGA_EnemySlash::OnMontageEnded);
-	MontageTask->ReadyForActivation();
 }
 
 void UKRGA_EnemySlash::OnMontageEnded()
