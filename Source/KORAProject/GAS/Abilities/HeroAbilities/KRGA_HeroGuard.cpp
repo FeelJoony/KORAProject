b3d7dbe448a992a4da447e9fcdde321bfd9809e1
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-11 19:19:42 +0900

    "update: 가드 GA 로직 리팩토링 및 새로운 이벤트 처리 추가"

--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroGuard.cpp
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroGuard.cpp
@@ -1,118 +1,39 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "GAS/Abilities/HeroAbilities/KRGA_HeroGuard.h"
-#include "AbilitySystemComponent.h"
-#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
-#include "GameFramework/Character.h"
-#include "GameFramework/CharacterMovementComponent.h"
+
+
+UKRGA_HeroGuard::UKRGA_HeroGuard(const FObjectInitializer& ObjectInitializer)
+{
+}
 
 void UKRGA_HeroGuard::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-
-	CachedPlayerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
-	if (!CachedPlayerCharacter)
-	{
-		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-		return;
-	}
-
-	if (UCharacterMovementComponent* MovementComp = CachedPlayerCharacter->GetCharacterMovement())
-	{
-		PreviousMaxWalkSpeed = MovementComp->MaxWalkSpeed;
-		MovementComp->MaxWalkSpeed = GuardMaxWalkSpeed;
-	}
-
-	LockCameraRotation();
-
-	if (GuardStartMontage)
-	{
-		StartMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-			this, TEXT("GuardStartMontage"), GuardStartMontage
-		);
-
-		StartMontageTask->OnCompleted.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		StartMontageTask->OnInterrupted.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		StartMontageTask->OnCancelled.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		StartMontageTask->OnBlendOut.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-
-		StartMontageTask->ReadyForActivation();
-	}
-	else
-	{
-		OnGuardMontageLoop();
-	}
 }
 
 void UKRGA_HeroGuard::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	UnlockCameraRotation();
-
-	if (IsValid(LoopMontageTask))
-	{
-		LoopMontageTask->EndTask();
-	}
-	if (IsValid(HitMontageTask))
-	{
-		HitMontageTask->EndTask();
-	}
-	if (CachedPlayerCharacter && CachedPlayerCharacter->GetCharacterMovement())
-	{
-		CachedPlayerCharacter->GetCharacterMovement()->MaxWalkSpeed = PreviousMaxWalkSpeed;
-	}
-
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
-void UKRGA_HeroGuard::InputReleased(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+void UKRGA_HeroGuard::OnGuardInputReleased(float TimeHeld)
 {
-	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
-	
-	if (IsValid(LoopMontageTask))
-	{
-		LoopMontageTask->EndTask();
-	}
-
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-
 }
 
-void UKRGA_HeroGuard::OnGuardMontageLoop()
+void UKRGA_HeroGuard::OnGuardBrokenEventReceived(FGameplayEventData Payload)
 {
-	if (GuardLoopMontage)
-	{
-		LoopMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-			this, TEXT("GuardLoopMontage"), GuardLoopMontage
-		);
-
-		LoopMontageTask->OnCompleted.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		LoopMontageTask->OnInterrupted.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		LoopMontageTask->OnCancelled.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-		LoopMontageTask->OnBlendOut.AddDynamic(this, &UKRGA_HeroGuard::OnGuardMontageLoop);
-
-		LoopMontageTask->ReadyForActivation();
-	}
 }
 
-void UKRGA_HeroGuard::LockCameraRotation()
+void UKRGA_HeroGuard::OnParrySuccessEventReceived(FGameplayEventData Payload)
 {
-	if (!CachedPlayerCharacter) return;
-
-	bPreviousUseControllerRotationYaw = CachedPlayerCharacter->bUseControllerRotationYaw;
-	bPreviousOrientRotationToMovement = CachedPlayerCharacter->GetCharacterMovement()->bOrientRotationToMovement;
-	
-	CachedPlayerCharacter->bUseControllerRotationYaw = false;
-	CachedPlayerCharacter->GetCharacterMovement()->bOrientRotationToMovement = false;
 }
 
-void UKRGA_HeroGuard::UnlockCameraRotation()
+void UKRGA_HeroGuard::StartGuardState()
 {
-	if (!CachedPlayerCharacter) return;
+}
 
-	CachedPlayerCharacter->bUseControllerRotationYaw = bPreviousUseControllerRotationYaw;
-	CachedPlayerCharacter->GetCharacterMovement()->bOrientRotationToMovement = bPreviousOrientRotationToMovement;
+void UKRGA_HeroGuard::StopGuardState()
+{
 }
--- Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroGuard.h
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/KRGA_HeroGuard.h
@@ -1,12 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GAS/Abilities/KRGameplayAbility.h"
 #include "KRGA_HeroGuard.generated.h"
 
-class UAbilityTask_PlayMontageAndWait;
+class UAbilityTask_WaitInputRelease;
+class UAbilityTask_WaitGameplayEvent;
 
 UCLASS()
 class KORAPROJECT_API UKRGA_HeroGuard : public UKRGameplayAbility
@@ -14,44 +13,38 @@ class KORAPROJECT_API UKRGA_HeroGuard : public UKRGameplayAbility
 	GENERATED_BODY()
 
 public:
+	UKRGA_HeroGuard(const FObjectInitializer& ObjectInitializer);
+
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
-	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
 
 protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Guard|Animation")
+	TObjectPtr<UAnimMontage> GuardMontage;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Guard|Animation")
+	TObjectPtr<UAnimMontage> GuardBreakMontage;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_WaitInputRelease> InputReleaseTask;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_WaitGameplayEvent> GuardBrokenEventTask;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_WaitGameplayEvent> ParrySuccessEventTask;
+
 	UFUNCTION()
-	void OnGuardMontageLoop();
+	void OnGuardInputReleased(float TimeHeld);
 
-	void LockCameraRotation();
-	void UnlockCameraRotation();
+	UFUNCTION()
+	void OnGuardBrokenEventReceived(FGameplayEventData Payload);
 
-private:
-
-	UPROPERTY(EditDefaultsOnly, Category = "Guard|Animation")
-	TObjectPtr<UAnimMontage> GuardStartMontage = nullptr;
+	UFUNCTION()
+	void OnParrySuccessEventReceived(FGameplayEventData Payload);
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Guard|Animation")
-	TObjectPtr<UAnimMontage> GuardLoopMontage = nullptr;
+	void StartGuardState();
+	void StopGuardState();
 
-	UPROPERTY(EditDefaultsOnly, Category = "Guard|Animation")
-	TObjectPtr<UAnimMontage> GuardHitMontage = nullptr;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Guard")
-	float GuardMaxWalkSpeed = 300.0f;
-	
-	UPROPERTY()
-	TObjectPtr<UAbilityTask_PlayMontageAndWait> StartMontageTask = nullptr;
-	UPROPERTY()
-	TObjectPtr<UAbilityTask_PlayMontageAndWait> LoopMontageTask = nullptr;
-	UPROPERTY()
-	TObjectPtr<UAbilityTask_PlayMontageAndWait> EndMontageTask = nullptr;
-	UPROPERTY()
-	TObjectPtr<UAbilityTask_PlayMontageAndWait> HitMontageTask = nullptr;
-
-	UPROPERTY()
-	TObjectPtr<ACharacter> CachedPlayerCharacter = nullptr;
-
-	bool bPreviousUseControllerRotationYaw = false;
-	bool bPreviousOrientRotationToMovement = false;
-	float PreviousMaxWalkSpeed = 0.0f;
 };
