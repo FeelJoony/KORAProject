1204d400f32f892f3d74c75516fd494ca458663b
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-12-24 17:07:12 +0900

    "update: 차지 공격 GA 및 풀차지 처리 로직 추가"

--- Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_ChargeAttack.cpp
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_ChargeAttack.cpp
@@ -1,36 +1,37 @@
-﻿#include "KRGameplayAbility_ChargeAttack.h"
-
-#include "IMediaControls.h"
-#include "Weapons/KRWeaponInstance.h"
-#include "Components/KRCombatComponent.h"
-#include "GameFramework/Character.h"
+#include "KRGameplayAbility_ChargeAttack.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
-#include "Abilities/Tasks/AbilityTask_WaitInputRelease.h"
+#include "Equipment/KREquipmentManagerComponent.h"
 #include "GAS/Abilities/Tasks/KRAbilityTask_WaitTick.h"
+#include "GAS/Abilities/HeroAbilities/Libraries/KRMotionWarpingLibrary.h"
+#include "GameFramework/Character.h"
 
-const FName UKRGameplayAbility_ChargeAttack::SECTION_START   = FName("Start");
-const FName UKRGameplayAbility_ChargeAttack::SECTION_FAIL    = FName("Attack_Fail");
+const FName UKRGameplayAbility_ChargeAttack::SECTION_START = FName("Start");
+const FName UKRGameplayAbility_ChargeAttack::SECTION_FAIL = FName("Attack_Fail");
 const FName UKRGameplayAbility_ChargeAttack::SECTION_SUCCESS = FName("Attack_Success");
 
 UKRGameplayAbility_ChargeAttack::UKRGameplayAbility_ChargeAttack(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	CurrentDamageMultiplier = 1.f;
-	DamageMulti_Fail = 1.f;
-	DamageMulti_Success = 2.f;
-	bIsChargeSuccess = false;
-	bIsFullyCharged = false;
-	bIsCharging = false;
-	TargetChargeTime = 0.f;
-	CurrentChargeTime = 0.f;
+	// 차지 공격 기본 설정
+	DefaultAttackConfig.AttackShape = EKRAttackShapeType::Cone;
+	DefaultAttackConfig.AttackRange = 250.0f;
+	DefaultAttackConfig.ConeAngle = 120.0f;
+	DefaultAttackConfig.DamageMultiplier = 1.0f;  // InputReleased에서 동적으로 변경
+	DefaultAttackConfig.HitIntensity = EKRHitIntensity::Heavy;
+	DefaultAttackConfig.bUseHitStop = true;
+	DefaultAttackConfig.HitStopDuration = 0.08f;
 }
 
-void UKRGameplayAbility_ChargeAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+void UKRGameplayAbility_ChargeAttack::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
 {
 	CheckComboTimeout();
-	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	// AttackBase의 ActivateAbility를 직접 호출 (MeleeAttack의 자동 몽타주 재생 건너뛰기)
+	UKRGameplayAbility_AttackBase::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
 	{
@@ -38,74 +39,85 @@ void UKRGameplayAbility_ChargeAttack::ActivateAbility(const FGameplayAbilitySpec
 		return;
 	}
 
-	/*UKRWeaponInstance* Weapon = nullptr;
-	if (UKRCombatComponent* CombatComp = GetCombatComponentFromActorInfo())
-	{
-		Weapon = CombatComp->GetCurrentWeaponInstance();
-	}
-
-	if (!Weapon)
-	{
-		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
-		return;
-	}*/
-
-	/*UAnimMontage* MontageToPlay = Weapon->GetChargeAttackMontage(CurrentComboIndex);
-
-	if (!MontageToPlay)
+	// 몽타주 조회
+	CurrentChargeMontage = GetCurrentMontage();
+	if (!CurrentChargeMontage)
 	{
 		ResetCombo();
-		MontageToPlay = Weapon->GetChargeAttackMontage(CurrentComboIndex);
+		CurrentChargeMontage = GetCurrentMontage();
 
-		if (!MontageToPlay)
+		if (!CurrentChargeMontage)
 		{
-			EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+			UE_LOG(LogTemp, Warning, TEXT("[ChargeAttack] No montage found for combo index %d"), CurrentComboIndex);
+			EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 			return;
 		}
 	}
 
-	CurrentChargeMontage = MontageToPlay;
+	// 히트 대상 초기화
+	HitActorsThisSwing.Empty();
+	bIsHitCheckActive = false;
 
 	// 차지 상태 초기화
 	bIsChargeSuccess = false;
 	bIsFullyCharged = false;
 	bIsCharging = true;
-	CurrentChargeTime = 0.f;
-	CurrentDamageMultiplier = DamageMulti_Fail;
+	CurrentChargeTime = 0.0f;
 
-	TargetChargeTime = Weapon->GetWeaponChargeTime();
-	if (TargetChargeTime <= KINDA_SMALL_NUMBER)
-	{
-		TargetChargeTime = 1.f;
-	}*/
-	
+	// 차지 페이즈 몽타주 재생 (Start 섹션부터)
 	ChargePlayTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
 		this,
 		FName("ChargeAttack_Charge"),
 		CurrentChargeMontage,
-		1.f,
+		1.0f,
 		SECTION_START,
-		false);
+		false  // bStopWhenAbilityEnds = false (수동으로 제어)
+	);
 
 	if (ChargePlayTask)
 	{
+		ChargePlayTask->OnCompleted.AddDynamic(this, &ThisClass::OnChargePhaseCompleted);
+		ChargePlayTask->OnInterrupted.AddDynamic(this, &ThisClass::OnChargePhaseInterrupted);
+		ChargePlayTask->OnCancelled.AddDynamic(this, &ThisClass::OnChargePhaseInterrupted);
 		ChargePlayTask->ReadyForActivation();
 	}
 
+	// 차지 틱 태스크 시작
 	ChargeTickTask = UKRAbilityTask_WaitTick::WaitTick(this, FName("ChargeTick"));
 	if (ChargeTickTask)
 	{
 		ChargeTickTask->OnTick.AddDynamic(this, &ThisClass::OnChargeTick);
 		ChargeTickTask->ReadyForActivation();
 	}
-	
+
 	IncrementCombo();
 }
 
-void UKRGameplayAbility_ChargeAttack::EndAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	bool bReplicateEndAbility, bool bWasCancelled)
+void UKRGameplayAbility_ChargeAttack::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
 {
+	// 차지 틱 태스크 정리
+	if (ChargeTickTask)
+	{
+		ChargeTickTask->EndTask();
+		ChargeTickTask = nullptr;
+	}
+
+	// 모션 워핑 타겟 정리
+	if (bUseMotionWarping)
+	{
+		UKRMotionWarpingLibrary::RemoveWarpTarget(GetAvatarActorFromActorInfo(), WarpTargetName);
+	}
+
+	// 히트 체크 상태 초기화
+	bIsHitCheckActive = false;
+	HitActorsThisSwing.Empty();
+	bIsCharging = false;
+
 	if (!bWasCancelled)
 	{
 		StartComboResetTimer();
@@ -114,11 +126,15 @@ void UKRGameplayAbility_ChargeAttack::EndAbility(const FGameplayAbilitySpecHandl
 	{
 		ResetCombo();
 	}
-	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	// AttackBase의 EndAbility 직접 호출
+	UKRGameplayAbility_AttackBase::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
-void UKRGameplayAbility_ChargeAttack::InputReleased(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+void UKRGameplayAbility_ChargeAttack::InputReleased(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo)
 {
 	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
 
@@ -129,14 +145,17 @@ void UKRGameplayAbility_ChargeAttack::InputReleased(const FGameplayAbilitySpecHa
 
 	bIsCharging = false;
 
+	// 차지 틱 태스크 종료
 	if (ChargeTickTask)
 	{
 		ChargeTickTask->EndTask();
 		ChargeTickTask = nullptr;
 	}
 
+	// 차지 실패 - 일찍 놓음
 	bIsChargeSuccess = false;
-	CurrentDamageMultiplier = DamageMulti_Fail;
+	DefaultAttackConfig.DamageMultiplier = DamageMulti_Fail;
+	DefaultAttackConfig.HitIntensity = EKRHitIntensity::Medium;
 
 	StartReleaseMontage(false);
 }
@@ -150,41 +169,26 @@ void UKRGameplayAbility_ChargeAttack::OnChargeTick(float DeltaTime)
 
 	CurrentChargeTime += DeltaTime;
 
+	// 풀 차지 달성
 	if (!bIsFullyCharged && CurrentChargeTime >= TargetChargeTime)
 	{
 		bIsFullyCharged = true;
-		bIsChargeSuccess =true;
+		bIsChargeSuccess = true;
 		bIsCharging = false;
-		CurrentDamageMultiplier = DamageMulti_Success;
 
+		DefaultAttackConfig.DamageMultiplier = DamageMulti_Success;
+		DefaultAttackConfig.HitIntensity = EKRHitIntensity::Heavy;
+
+		// 차지 틱 태스크 종료
 		if (ChargeTickTask)
 		{
 			ChargeTickTask->EndTask();
 			ChargeTickTask = nullptr;
 		}
 
+		// 차지 성공 - 자동 릴리즈
 		StartReleaseMontage(true);
 	}
-}
-
-
-
-void UKRGameplayAbility_ChargeAttack::OnMontageCompleted()
-{
-	if (!IsActive())
-	{
-		return;
-	}
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-}
-
-void UKRGameplayAbility_ChargeAttack::OnMontageInterrupted()
-{
-	if (!IsActive())
-	{
-		return;
-	}
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 }
 
 void UKRGameplayAbility_ChargeAttack::StartReleaseMontage(bool bSuccess)
@@ -192,15 +196,18 @@ void UKRGameplayAbility_ChargeAttack::StartReleaseMontage(bool bSuccess)
 	ACharacter* Char = Cast<ACharacter>(GetAvatarActorFromActorInfo());
 	if (!Char || !CurrentChargeMontage)
 	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
 		return;
 	}
 
+	// 차지 페이즈 몽타주 정리
 	if (ChargePlayTask)
 	{
 		ChargePlayTask->EndTask();
 		ChargePlayTask = nullptr;
 	}
-	
+
+	// 현재 재생 중인 몽타주 중지
 	if (UAnimInstance* AnimInst = Char->GetMesh() ? Char->GetMesh()->GetAnimInstance() : nullptr)
 	{
 		if (AnimInst->Montage_IsPlaying(CurrentChargeMontage))
@@ -209,22 +216,69 @@ void UKRGameplayAbility_ChargeAttack::StartReleaseMontage(bool bSuccess)
 		}
 	}
 
+	// 모션 워핑 설정 (릴리즈 시점에)
+	if (bUseMotionWarping)
+	{
+		SetupMotionWarping();
+	}
+
+	// 릴리즈 페이즈 몽타주 재생
 	const FName ReleaseSection = bSuccess ? SECTION_SUCCESS : SECTION_FAIL;
 
 	ReleasePlayTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
 		this,
 		FName("ChargeAttack_Release"),
 		CurrentChargeMontage,
-		1.f,
+		1.0f,
 		ReleaseSection,
-		true);
+		true
+	);
 
 	if (ReleasePlayTask)
 	{
-		ReleasePlayTask->OnCompleted.AddDynamic(this, &ThisClass::OnMontageCompleted);
-		ReleasePlayTask->OnInterrupted.AddDynamic(this, &ThisClass::OnMontageInterrupted);
-		ReleasePlayTask->OnCancelled.AddDynamic(this, &ThisClass::OnMontageInterrupted);
-		ReleasePlayTask->OnBlendOut.AddDynamic(this, &ThisClass::OnMontageCompleted);
+		ReleasePlayTask->OnCompleted.AddDynamic(this, &ThisClass::OnReleasePhaseCompleted);
+		ReleasePlayTask->OnInterrupted.AddDynamic(this, &ThisClass::OnReleasePhaseInterrupted);
+		ReleasePlayTask->OnCancelled.AddDynamic(this, &ThisClass::OnReleasePhaseInterrupted);
+		ReleasePlayTask->OnBlendOut.AddDynamic(this, &ThisClass::OnReleasePhaseCompleted);
 		ReleasePlayTask->ReadyForActivation();
 	}
+}
+
+void UKRGameplayAbility_ChargeAttack::OnChargePhaseCompleted()
+{
+	// 차지 페이즈가 완료되면 (Start 섹션 끝) 릴리즈로 전환
+	// 일반적으로 InputReleased나 풀차지에서 먼저 처리됨
+}
+
+void UKRGameplayAbility_ChargeAttack::OnChargePhaseInterrupted()
+{
+	if (!IsActive())
+	{
+		return;
+	}
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+void UKRGameplayAbility_ChargeAttack::OnReleasePhaseCompleted()
+{
+	if (!IsActive())
+	{
+		return;
+	}
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UKRGameplayAbility_ChargeAttack::OnReleasePhaseInterrupted()
+{
+	if (!IsActive())
+	{
+		return;
+	}
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+const TArray<TObjectPtr<UAnimMontage>>* UKRGameplayAbility_ChargeAttack::GetMontageArrayFromEntry(
+	const FKRAppliedEquipmentEntry& Entry) const
+{
+	return &Entry.GetChargeAttackMontages();
 }
--- Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_ChargeAttack.h
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_ChargeAttack.h
@@ -1,14 +1,20 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
-#include "KRGameplayAbility_MeleeBase.h"
+#include "KRGameplayAbility_MeleeAttack.h"
 #include "KRGameplayAbility_ChargeAttack.generated.h"
 
 class UAbilityTask_PlayMontageAndWait;
 class UKRAbilityTask_WaitTick;
 
+/**
+ * Charge Attack GA (차지 공격)
+ * - 버튼 홀드 시 차지
+ * - 차지 성공/실패에 따른 다른 공격
+ * - 새로운 Shape Trace 기반 히트 감지
+ */
 UCLASS()
-class KORAPROJECT_API UKRGameplayAbility_ChargeAttack : public UKRGameplayAbility_MeleeBase
+class KORAPROJECT_API UKRGameplayAbility_ChargeAttack : public UKRGameplayAbility_MeleeAttack
 {
 	GENERATED_BODY()
 
@@ -18,26 +24,47 @@ public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
-	
+
+protected:
+	/** ChargeAttack 몽타주 배열 반환 */
+	virtual const TArray<TObjectPtr<UAnimMontage>>* GetMontageArrayFromEntry(const FKRAppliedEquipmentEntry& Entry) const override;
+
+protected:
+	// ─────────────────────────────────────────────────────
+	// 차지 설정
+	// ─────────────────────────────────────────────────────
+
+	/** 차지 실패 시 데미지 배율 */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat|Charge")
+	float DamageMulti_Fail = 1.0f;
+
+	/** 차지 성공 시 데미지 배율 */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat|Charge")
+	float DamageMulti_Success = 2.0f;
+
+	/** 차지 완료에 필요한 시간 (초) */
+	UPROPERTY(EditDefaultsOnly, Category = "Combat|Charge")
+	float TargetChargeTime = 1.0f;
+
 protected:
 	UFUNCTION()
 	void OnChargeTick(float DeltaTime);
 
 	UFUNCTION()
-	void OnMontageCompleted();
+	void OnChargePhaseCompleted();
 
 	UFUNCTION()
-	void OnMontageInterrupted();
-	
+	void OnChargePhaseInterrupted();
+
+	UFUNCTION()
+	void OnReleasePhaseCompleted();
+
+	UFUNCTION()
+	void OnReleasePhaseInterrupted();
+
 	void StartReleaseMontage(bool bSuccess);
 
 private:
-	UPROPERTY(EditDefaultsOnly, Category = "Combat|Charge")
-	float DamageMulti_Fail;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Combat|Charge")
-	float DamageMulti_Success;
-
 	UPROPERTY()
 	TObjectPtr<UKRAbilityTask_WaitTick> ChargeTickTask;
 
@@ -49,16 +76,15 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<UAbilityTask_PlayMontageAndWait> ReleasePlayTask;
-	
+
 	// 차지 성공 여부
-	bool bIsChargeSuccess;
-	// 풀차지가 된 시점인지 (이펙트용)
-	bool bIsFullyCharged;
+	bool bIsChargeSuccess = false;
+	// 풀차지가 된 시점인지
+	bool bIsFullyCharged = false;
 	// 버튼을 누르고 있는 동안만 true
-	bool bIsCharging;
-	
-	float TargetChargeTime;
-	float CurrentChargeTime;
+	bool bIsCharging = false;
+
+	float CurrentChargeTime = 0.0f;
 
 	static const FName SECTION_START;
 	static const FName SECTION_FAIL;
