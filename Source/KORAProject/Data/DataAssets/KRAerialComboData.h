080390c95e009b1fc324dcd0759edc5ffdefd884
LimHyeongGyun <200hk@naver.com>
2026-01-02 05:16:22 +0900

    AerialCombo Effect
    1.AerialCombo Effect 수정
    2.AerialComboData 불필요한 코드 제거

--- Source/KORAProject/Data/DataAssets/KRAerialComboData.h
+++ Source/KORAProject/Data/DataAssets/KRAerialComboData.h
@@ -11,41 +11,6 @@ class UGameplayEffect;
 class UKRCameraMode;
 
 USTRUCT(BlueprintType)
-struct FKRAerialComboLaunchSettings
-{
-	GENERATED_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Launch", meta = (ClampMin = "100.0", ClampMax = "1000.0"))
-	float EnemyLaunchHeight = 500.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Launch", meta = (ClampMin = "500.0", ClampMax = "2000.0"))
-	float EnemyLaunchVelocity = 1200.0f;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Launch", meta = (ClampMin = "500.0", ClampMax = "2000.0"))
-	float PlayerJumpVelocity = 1000.0f;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Launch", meta = (ClampMin = "0.1", ClampMax = "1.0"))
-	float AirborneHangTime = 0.3f;
-};
-
-USTRUCT(BlueprintType)
-struct FKRAerialComboSlamSettings
-{
-	GENERATED_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slam", meta = (ClampMin = "1000.0", ClampMax = "5000.0"))
-	float SlamVelocity = 2000.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slam", meta = (ClampMin = "100.0", ClampMax = "500.0"))
-	float ImpactRadius = 300.0f;
-
-	/** 바운스 높이 (적이 땅에 부딪힐 때) */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slam", meta = (ClampMin = "0.0", ClampMax = "300.0"))
-	float BounceHeight = 100.0f;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slam", meta = (ClampMin = "0.0", ClampMax = "0.5"))
-	float HitStopDuration = 0.15f;
-};
-
-USTRUCT(BlueprintType)
 struct FKRAerialComboDamageSettings
 {
 	GENERATED_BODY()
@@ -70,17 +35,7 @@ struct FKRAerialComboMontageSettings
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
-	TObjectPtr<UAnimMontage> LaunchMontage = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
-	TObjectPtr<UAnimMontage> RisingMontage = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
-	TObjectPtr<UAnimMontage> AirborneMontage = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
-	TObjectPtr<UAnimMontage> SlamMontage = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
-	TObjectPtr<UAnimMontage> LandingMontage = nullptr;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage", meta = (ClampMin = "0.5", ClampMax = "2.0"))
-	float MontagePlayRate = 1.0f;
+	TObjectPtr<UAnimMontage> AerialMontage = nullptr;
 };
 
 USTRUCT(BlueprintType)
@@ -91,13 +46,11 @@ struct FKRAerialComboEffectSettings
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EffectTag", meta = (Categories = "Effect"))
 	FGameplayTag LaunchWindEffectTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EffectTag", meta = (Categories = "Effect"))
-	FGameplayTag AfterImageEffectTag;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EffectTag", meta = (Categories = "Effect"))
-	FGameplayTag SlamTrailEffectTag;
+	FGameplayTag LineTrailEffectTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EffectTag", meta = (Categories = "Effect"))
 	FGameplayTag ImpactEffectTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EffectTag", meta = (Categories = "Effect"))
-	FGameplayTag GroundCrackEffectTag;
+	FGameplayTag FinishEffectTag;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Params")
 	FLinearColor EffectColor = FLinearColor(0.8f, 0.2f, 0.1f, 1.0f);
@@ -113,43 +66,20 @@ struct FKRAerialComboSoundSettings
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SoundTag", meta = (Categories = "Sound"))
 	FGameplayTag LaunchSoundTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SoundTag", meta = (Categories = "Sound"))
-	FGameplayTag WindSoundTag;
+	FGameplayTag DashSoundTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SoundTag", meta = (Categories = "Sound"))
-	FGameplayTag SlamSoundTag;
+	FGameplayTag HitSoundTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SoundTag", meta = (Categories = "Sound"))
-	FGameplayTag ImpactSoundTag;
+	FGameplayTag FinishSoundTag;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayCue", meta = (Categories = "GameplayCue"))
 	FGameplayTag LaunchCueTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayCue", meta = (Categories = "GameplayCue"))
-	FGameplayTag RisingCueTag;
+	FGameplayTag DashCueTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayCue", meta = (Categories = "GameplayCue"))
-	FGameplayTag SlamCueTag;
+	FGameplayTag HitCueTag;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GameplayCue", meta = (Categories = "GameplayCue"))
-	FGameplayTag ImpactCueTag;
-};
-
-USTRUCT(BlueprintType)
-struct FKRAerialComboMotionWarpingSettings
-{
-	GENERATED_BODY()
-
-	/** 런칭 워프 타겟 이름 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MotionWarping")
-	FName LaunchWarpTargetName = FName("LaunchTarget");
-	/** 공중 추적 워프 타겟 이름 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MotionWarping")
-	FName AirborneWarpTargetName = FName("AirborneTarget");
-	/** 슬램 워프 타겟 이름 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MotionWarping")
-	FName SlamWarpTargetName = FName("SlamTarget");
-
-	/** 런칭 시 적과의 거리 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MotionWarping", meta = (ClampMin = "50.0", ClampMax = "200.0"))
-	float LaunchApproachDistance = 100.0f;
-	/** 슬램 시 적 위 오프셋 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MotionWarping", meta = (ClampMin = "50.0", ClampMax = "300.0"))
-	float SlamHeightOffset = 150.0f;
+	FGameplayTag FinishCueTag;
 };
 
 USTRUCT(BlueprintType)
@@ -186,19 +116,6 @@ struct FKRAerialComboCameraSettings
 	float CameraShakeScale = 1.0f;
 };
 
-USTRUCT(BlueprintType)
-struct FKRAerialComboPhysicsSettings
-{
-	GENERATED_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float AirborneGravityScale = 0.3f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics", meta = (ClampMin = "1.0", ClampMax = "5.0"))
-	float SlamGravityScale = 3.0f;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics", meta = (ClampMin = "0.0", ClampMax = "1.0"))
-	float EnemyAirborneGravityScale = 0.1f;
-};
-
 UCLASS(BlueprintType)
 class KORAPROJECT_API UKRAerialComboData : public UPrimaryDataAsset
 {
@@ -209,11 +126,7 @@ public:
 	FText DisplayName;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info", meta = (MultiLine = true))
 	FText Description;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Launch")
-	FKRAerialComboLaunchSettings Launch;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Slam")
-	FKRAerialComboSlamSettings Slam;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
 	FKRAerialComboDamageSettings Damage;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Montage")
@@ -222,21 +135,17 @@ public:
 	FKRAerialComboEffectSettings Effects;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound")
 	FKRAerialComboSoundSettings Sound;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MotionWarping")
-	FKRAerialComboMotionWarpingSettings MotionWarping;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Targeting")
 	FKRAerialComboTargetingSettings Targeting;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
 	FKRAerialComboCameraSettings Camera;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Physics")
-	FKRAerialComboPhysicsSettings Physics;
 	
-	UFUNCTION(BlueprintPure, Category = "AerialCombo")
-	float GetEstimatedDuration() const
-	{
-		// Launch + Airborne + Slam + Landing 대략적 시간
-		return 2.0f + Launch.AirborneHangTime + Slam.HitStopDuration;
-	}
+	//UFUNCTION(BlueprintPure, Category = "AerialCombo")
+	//float GetEstimatedDuration() const
+	//{
+	//	// Launch + Airborne + Slam + Landing 대략적 시간
+	//	return 2.0f + Launch.AirborneHangTime + Slam.HitStopDuration;
+	//}
 	
 	virtual FPrimaryAssetId GetPrimaryAssetId() const override
 	{
