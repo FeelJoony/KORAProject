aab4e6a2a77c8d61744a32689dca2cd301ee25eb
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2026-01-09 03:21:46 +0900

    "update: HeroComponent 이동 로직 최적화 및 카메라 모드 설정 추가"

--- Source/KORAProject/Components/KRHeroComponent.cpp
+++ Source/KORAProject/Components/KRHeroComponent.cpp
@@ -182,8 +182,6 @@ void UKRHeroComponent::InitializePlayerInput(UInputComponent* PlayerInputCompone
 
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = KRLP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
 	check(Subsystem)
-
-	//Subsystem->ClearAllMappings();
 	
 	if (const UKRPawnExtensionComponent* PawnExtComp = UKRPawnExtensionComponent::FindPawnExtensionComponent(Pawn))
 	{
@@ -214,18 +212,6 @@ void UKRHeroComponent::Input_Move(const FInputActionValue& InputActionValue)
 	APawn* Pawn = GetPawn<APawn>();
 	if (!Pawn) return;
 
-	// 이동 제한 태그 확인
-	if (AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>())
-	{
-		if (UKRAbilitySystemComponent* ASC = KRPS->GetKRAbilitySystemComponent())
-		{
-			if (ASC->HasMatchingGameplayTag(KRTAG_STATE_RESTRICT_MOVEMENT))
-			{
-				return;
-			}
-		}
-	}
-
 	AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>();
 	if (KRPS)
 	{
@@ -237,6 +223,19 @@ void UKRHeroComponent::Input_Move(const FInputActionValue& InputActionValue)
 			}
 		}
 	}
+	
+	if (AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>())
+	{
+		if (UKRAbilitySystemComponent* ASC = KRPS->GetKRAbilitySystemComponent())
+		{
+			if (ASC->HasMatchingGameplayTag(KRTAG_STATE_RESTRICT_MOVEMENT))
+			{
+				return;
+			}
+		}
+	}
+
+	
 	
 	AController* PC = Pawn->GetController();
 
@@ -271,9 +270,8 @@ void UKRHeroComponent::Input_Look(const FInputActionValue& InputActionValue)
 	AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>();
 	if (UKRAbilitySystemComponent* ASC = KRPS->GetKRAbilitySystemComponent())
 	{
-		if (ASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag("State.Acting.LockOn")))
+		if (ASC->HasMatchingGameplayTag(KRTAG_STATE_ACTING_LOCKON))
 		{
-			LastLookInput = InputActionValue.Get<FVector2D>();
 			return;
 		}
 	}
@@ -293,11 +291,7 @@ void UKRHeroComponent::Input_Look(const FInputActionValue& InputActionValue)
 
 void UKRHeroComponent::Input_AbilityInputPressed(FGameplayTag InputTag)
 {
-	APawn* Pawn = GetPawn<APawn>();
-	if (!Pawn) return;
-
-	const AKRPlayerState* KRPS = Pawn->GetPlayerState<AKRPlayerState>();
-	if (KRPS)
+	if (const AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>())
 	{
 		if (UKRAbilitySystemComponent* KRASC = KRPS->GetKRAbilitySystemComponent())
 		{
@@ -308,11 +302,7 @@ void UKRHeroComponent::Input_AbilityInputPressed(FGameplayTag InputTag)
 
 void UKRHeroComponent::Input_AbilityInputReleased(FGameplayTag InputTag)
 {
-	APawn* Pawn = GetPawn<APawn>();
-	if (!Pawn) return;
-
-	const AKRPlayerState* KRPS = Pawn->GetPlayerState<AKRPlayerState>();
-	if (KRPS)
+	if (const AKRPlayerState* KRPS = GetPlayerState<AKRPlayerState>())
 	{
 		if (UKRAbilitySystemComponent* KRASC = KRPS->GetKRAbilitySystemComponent())
 		{
@@ -351,4 +341,9 @@ TSubclassOf<UKRCameraMode> UKRHeroComponent::DetermineCameraMode() const
 	}
 
 	return nullptr;
+}
+
+void UKRHeroComponent::SetAbilityCameraMode(TSubclassOf<UKRCameraMode> InCameraMode)
+{
+	AbilityCameraMode = InCameraMode;
 }
