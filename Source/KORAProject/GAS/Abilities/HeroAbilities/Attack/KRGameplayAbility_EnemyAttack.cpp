cfc0d3fc4f4e35a0a0447777347944da7806c535
n-fia <78014168+n-fia@users.noreply.github.com>
2026-01-10 06:09:58 +0900

    Enemy GA 추가/수정, BP 추가/수정, 몽타주 수정

---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_EnemyAttack.cpp
@@ -0,0 +1,96 @@
+#include "GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_EnemyAttack.h"
+
+#include "AbilitySystemComponent.h"
+#include "Components/StateTreeAIComponent.h"
+
+void UKRGameplayAbility_EnemyAttack::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                                     const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                                     const FGameplayEventData* TriggerEventData)
+{
+	AppliedEffects.Empty();
+	BeginOnlyTriggerOnce=false;
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+	if (MontageToPlay != nullptr && AnimInstance != nullptr && AnimInstance->GetActiveMontageInstance() == nullptr)
+	{
+		TArray<const UGameplayEffect*> Effects;
+		
+		for (TSubclassOf<UGameplayEffect> EffectClass : GameplayEffectClassesWhileAnimating)
+		{
+			if (EffectClass)
+			{
+				Effects.Add(EffectClass->GetDefaultObject<UGameplayEffect>());
+			}
+		}
+
+		if (Effects.Num() > 0)
+		{
+			UAbilitySystemComponent* const AbilitySystemComponent = ActorInfo->AbilitySystemComponent.Get();
+			for (const UGameplayEffect* Effect : Effects)
+			{
+				FActiveGameplayEffectHandle EffectHandle = AbilitySystemComponent->ApplyGameplayEffectToSelf(Effect, 1.f, MakeEffectContext(Handle, ActorInfo));
+				if (EffectHandle.IsValid())
+				{
+					AppliedEffects.Add(EffectHandle);
+				}
+			}
+		}
+	}
+	
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (ActorInfo->OwnerActor.IsValid())
+	{
+		if (AActor* OwnerActor = ActorInfo->OwnerActor.Get())
+		{
+			if (UStateTreeAIComponent* StateTreeAIComp = OwnerActor->GetComponentByClass<UStateTreeAIComponent>())
+			{
+				if (AbilityTags.Num() <= 0)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Montage AbilityTags is Empty"));
+				}
+
+				StateTreeAIComp->SendStateTreeEvent(AbilityTags.GetByIndex(0));
+			}
+		}
+	}
+	
+	//UE_LOG(LogTemp, Warning, TEXT("Active Ability"));
+}
+
+void UKRGameplayAbility_EnemyAttack::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	if (ActorInfo->OwnerActor.IsValid())
+	{
+		if (AActor* OwnerActor = ActorInfo->OwnerActor.Get())
+		{
+			if (UStateTreeAIComponent* StateTreeAIComp = OwnerActor->GetComponentByClass<UStateTreeAIComponent>())
+			{
+				if (EndAbilityTags.Num() <= 0)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Montage AbilityTags is Empty"));
+				}
+
+				StateTreeAIComp->SendStateTreeEvent(EndAbilityTags.GetByIndex(0));
+			}
+		}
+	}
+
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponentFromActorInfo();;
+	if (ASC)
+	{
+		for (FActiveGameplayEffectHandle& Effect : AppliedEffects)
+		{
+			ASC->RemoveActiveGameplayEffect(Effect);
+		}
+	}
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+UAnimMontage* UKRGameplayAbility_EnemyAttack::GetCurrentMontage() const
+{
+	return MontageToPlay;
+}
+
---
+++ Source/KORAProject/GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_EnemyAttack.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/HeroAbilities/Attack/KRGameplayAbility_MeleeAttack.h"
+#include "KRGameplayAbility_EnemyAttack.generated.h"
+
+UCLASS()
+class KORAPROJECT_API UKRGameplayAbility_EnemyAttack : public UKRGameplayAbility_MeleeAttack
+{
+	GENERATED_BODY()
+public:
+	//UKRGameplayAbility_EnemyAttack(const FObjectInitializer& ObjectInitializer);
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAnimMontage> MontageToPlay;
+	
+	UPROPERTY(EditDefaultsOnly, Category = MontageAbility)
+	TArray<TSubclassOf<UGameplayEffect>> GameplayEffectClassesWhileAnimating;
+protected:
+	virtual UAnimMontage* GetCurrentMontage() const override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Tags")
+	FGameplayTagContainer EndAbilityTags;
+
+	TArray<FActiveGameplayEffectHandle> AppliedEffects;
+};
