d8ef8d148df40a8f0945193e6da407f67b51f65a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2026-01-10 15:48:26 +0900

    "update: 체크포인트 저장 및 초기화 기능 추가"

--- Source/KORAProject/Game/KRGameInstance.h
+++ Source/KORAProject/Game/KRGameInstance.h
@@ -11,7 +11,28 @@ UCLASS()
 class KORAPROJECT_API UKRGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	/** 마지막으로 저장된 체크포인트 태그 (PlayerStart용) */
+	UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+	FName LastCheckpointTag;
+
+	/** 마지막으로 저장된 체크포인트 Transform (정확한 위치 스폰용) */
+	UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+	FTransform LastCheckpointTransform;
+
+	/** 체크포인트가 설정되었는지 여부 */
+	UPROPERTY(BlueprintReadWrite, Category = "SaveData")
+	bool bHasCheckpoint = false;
+
+	/** 체크포인트 저장 */
+	UFUNCTION(BlueprintCallable, Category = "Checkpoint")
+	void SaveCheckpoint(const FTransform& SpawnTransform, FName CheckpointTag = NAME_None);
+
+	/** 체크포인트 초기화 */
+	UFUNCTION(BlueprintCallable, Category = "Checkpoint")
+	void ClearCheckpoint();
+
 protected:
 	virtual void Init() override;
 };
