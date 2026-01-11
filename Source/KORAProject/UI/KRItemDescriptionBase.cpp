41ebbed984b532e0fa7713b283e9c0ae2b7d4e59
JuyeeMyeong <juyee.ming@gmail.com>
2026-01-11 19:15:48 +0900

    [feature] 죽었을 시 아이템 습득 UI 연결, 관련 UI ST 캐싱

--- Source/KORAProject/UI/KRItemDescriptionBase.cpp
+++ Source/KORAProject/UI/KRItemDescriptionBase.cpp
@@ -2,3 +2,29 @@
 
 
 #include "UI/KRItemDescriptionBase.h"
+#include "Internationalization/StringTableRegistry.h"
+
+const FName UKRItemDescriptionBase::ItemStringTableId(TEXT("ST_ItemList"));
+
+FText UKRItemDescriptionBase::GetLocalizedTextFromKey(const FName& InKey) const
+{
+	if (InKey.IsNone())
+	{
+		return FText::GetEmpty();
+	}
+
+	FStringTableConstPtr StringTable = FStringTableRegistry::Get().FindStringTable(ItemStringTableId);
+	if (!StringTable.IsValid())
+	{
+		return FText::FromName(InKey);
+	}
+
+	FText Result = FText::FromStringTable(ItemStringTableId, InKey.ToString());
+
+	if (Result.IsEmpty())
+	{
+		return FText::FromName(InKey);
+	}
+
+	return Result;
+}
--- Source/KORAProject/UI/KRItemSlotBase.h
+++ Source/KORAProject/UI/KRItemSlotBase.h
@@ -4,6 +4,8 @@
 
 #include "CommonButtonBase.h"
 #include "UI/Data/KRItemUIData.h"
+#include "Internationalization/StringTable.h"
+#include "UObject/SoftObjectPath.h"
 #include "KRItemSlotBase.generated.h"
 
 class UCommonTextBlock;
@@ -30,17 +32,20 @@ UCLASS()
 class KORAPROJECT_API UKRItemSlotBase : public UCommonButtonBase
 {
 	GENERATED_BODY()
-	
+
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot", meta = (Bitmask, BitmaskEnum = "/Script/KORAProject.EKRItemField"))
 	int32 VisibleFields = Bit(EKRItemField::Name) | Bit(EKRItemField::Description);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Localization")
+	TSoftObjectPtr<UStringTable> ItemStringTableAsset = TSoftObjectPtr<UStringTable>(FSoftObjectPath(TEXT("/Game/UI/StringTable/ST_ItemList.ST_ItemList")));
 
 	UFUNCTION(BlueprintCallable, Category = "Slot") void SetItemData(const FKRItemUIData& InData, int32 OverrideFields = -1);
 	UFUNCTION(BlueprintCallable, Category = "Slot") void ClearItemData();
 	UFUNCTION(BlueprintCallable) void SetHovered(bool bHovered);
 
 protected:
-	static const FName ItemStringTableId;
+	virtual void NativeConstruct() override;
 
 	UPROPERTY(meta = (BindWidgetOptional)) UCommonLazyImage* ItemIcon = nullptr;
 	UPROPERTY(meta = (BindWidgetOptional)) UCommonTextBlock* ItemName = nullptr;
@@ -50,12 +55,17 @@ protected:
 	UPROPERTY(meta = (BindWidgetOptional)) UCommonNumericTextBlock* ShopStock = nullptr;
 
 private:
-
 	static FORCEINLINE bool HasField(int32 M, EKRItemField F) { return (M & Bit(F)) != 0; }
 
 	void ApplyFieldsVisibility(int32 FieldsToShow);
 	static void SetWidgetVisible(UWidget* W, bool bShow);
 	static bool IsDataEmpty(const FKRItemUIData& D);
 
+	void EnsureStringTableLoaded();
 	FText GetSourceStringFromTable(const FName& InKey);
+
+	UPROPERTY(Transient)
+	TObjectPtr<UStringTable> CachedStringTable;
+
+	bool bStringTableLoaded = false;
 };
